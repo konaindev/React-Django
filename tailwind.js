2329bcc75a7eeec02291d9aa44e2e9a234124558
Choong Ng <choong@pioneersquarelabs.com>
2019-01-09 15:37:11 -0800

    Update background color to match mock.

--- tailwind.js
+++ tailwind.js
@@ -67,7 +67,7 @@ let colors = {
   // Modern UI colors
   "remark-ui-dark": "#1A202E",
   "remark-ui-border-dark": "#161C29",
-  "remark-ui-darker": "#1D1723",
+  "remark-ui-darker": "#121723",
   "remark-ui-darkest": "#0D111A",
   "remark-ui-text-lightest": "#FFFFFF",
   "remark-ui-text-light": "#cccccc",
