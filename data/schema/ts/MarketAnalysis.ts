20ba7bb0e46e4f981087b6fe0f73afc9b2d7715d
Dave Peck <davepeck@gmail.com>
2019-02-22 17:07:42 -0800

    Add estimated population mapping data to MarketAnalysis report.
    Adopt GeoJSON for all geometric data.

--- data/schema/ts/MarketAnalysis.ts
+++ data/schema/ts/MarketAnalysis.ts
@@ -1,4 +1,5 @@
 import * as t from "./types";
+import * as geo from "./geojson";
 
 /** Names of specific sub-populations we concern ourselves with in market analysis */
 enum ActivePopulation {
@@ -110,6 +111,44 @@ interface RentToIncome {
   data: (t.percent | null)[][];
 }
 
+/** Base interface for all estimated population values */
+interface EstimatedPopulation {
+  /** The total estimated population */
+  population: t.integer;
+}
+
+/** Geographic distance */
+enum DistanceUnits {
+  miles = "mi",
+  kilometers = "km"
+}
+
+/** Estimated population with a point and range */
+interface EstimatedPopulationRange extends EstimatedPopulation {
+  /** The geographic center of the population area, as a GeoJSON Point */
+  center: geo.Point;
+
+  /** The radius of the population area, in the defined units */
+  radius: t.float;
+
+  /** The unit used in the radius; can be used for display */
+  units: DistanceUnits;
+}
+
+/** Zip code details for a population */
+interface PopulationZipCode {
+  /** The zip code */
+  zip: string;
+
+  /** A polygon outlining the zipcode as a GeoJSON Polygon; may be null if unknown */
+  outline: geo.Polygon | geo.MultiPolygon | null;
+}
+
+/** Estimated population with a set of zip codes */
+interface EstimatedPopulationZipCodes extends EstimatedPopulation {
+  zip_codes: PopulationZipCode[];
+}
+
 export interface MarketAnalysis {
   /** The total population across all segments */
   total_population: t.integer;
@@ -119,10 +158,13 @@ export interface MarketAnalysis {
 
   // CONSIDER this is *nearly* duplicative data, but the important thing
   // to understand is that it selects the correct `market_size` value
-  // from each segment; the frontend shouldn't really do that selection.
+  // from each segment; the frontend shouldn't really perform that selection.
   /** Market size overview */
   market_sizes: MarketSize[];
 
   /** Provides an analysis of rent to income in the relevant brackets */
   rent_to_income: RentToIncome;
+
+  /** An estimated population value and technique */
+  estimated_population: EstimatedPopulationRange | EstimatedPopulationZipCodes;
 }
--- data/schema/ts/types.ts
+++ data/schema/ts/types.ts
@@ -9,13 +9,12 @@ export type date = string;
 export type datetime = string;
 export type integer = number;
 export type percent = number;
+export type float = number;
 
 /** Target values must match the underlying type, or explicitly be null */
 export type Targets<T> = {
   [P in keyof T]: T[P] extends object ? Targets<T[P]> : T[P] | null
 };
-
-/**  */
 
 //
 // Provide a common set of simple types for our schema to adopt.
