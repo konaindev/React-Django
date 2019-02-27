74346eba4d8b6beb9847f4bf0c8b7b98ee0832c1
mingthegit <ming.dev1026@gmail.com>
2019-02-27 17:13:17 +0800

    replaced table with flex grid

--- src/js/components/market_size_growth_reach/index.js
+++ src/js/components/market_size_growth_reach/index.js
@@ -29,57 +29,49 @@ export function MarketSizeGrowthReach({
 
       <div className="section__panel">
         <div className="panel__table">
-          <table>
-            <thead>
-              <tr>
-                <th>Target Segment</th>
-                <th>Est. Market Size</th>
-                <th>Unique Site Visitors</th>
-                <th>Est. Market Growth</th>
-                <th>Est. {future_year} Market Size</th>
-               </tr>
-            </thead>
-            <tbody>
-              {market_sizes.map((size, index) =>
-                <tr key={index}>
-                  <td>Ages {size.age_group.split('-').join(' - ')}</td>
-                  <td>{formatNumber(size.market_size)}</td>
-                  <td>{formatNumber(size.usv)}</td>
-                  <td>{formatPercent(size.growth, 2)}</td>
-                  <td>{formatNumber(size.future_size)}</td>
-                </tr>
-              )}
-            </tbody>
-          </table>
+          <div className="table__row table__row--head">
+            <span>Target Segment</span>
+            <span>Est. Market Size</span>
+            <span>Unique Site Visitors</span>
+            <span>Est. Market Growth</span>
+            <span>Est. {future_year} Market Size</span>
+          </div>
+          {market_sizes.map((size, index) =>
+            <div key={index} className="table__row table__row--body">
+              <span>Ages {size.age_group.split('-').join(' - ')}</span>
+              <span>{formatNumber(size.market_size)}</span>
+              <span>{formatNumber(size.usv)}</span>
+              <span>{formatPercent(size.growth, 2)}</span>
+              <span>{formatNumber(size.future_size)}</span>
+            </div>
+          )}
         </div>
 
         <div className="panel__summary">
-          <div className="summary__grid">
-            <div className="grid__box text-left">
-              <span>Average</span>
-              <span>Tenant Age</span>
-              <span>{age}</span>
-            </div>
-            <div className="grid__box text-right">
-              <span>Est. Total</span>
-              <span>Market Size</span>
-              <span>{formatNumber(market_size)}</span>
-            </div>
-            <div className="grid__box text-right">
-              <span>Total Unique</span>
-              <span>Site Visitors</span>
-              <span>{formatNumber(usv)}</span>
-            </div>
-            <div className="grid__box text-right">
-              <span>Average</span>
-              <span>Market Growth</span>
-              <span>{formatPercent(growth, 1)}</span>
-            </div>
-            <div className="grid__box text-right">
-              <span>Est. Total {future_year}</span>
-              <span>Market Size</span>
-              <span>{formatNumber(future_size)}</span>
-            </div>
+          <div className="grid__box text-left">
+            <span>Average</span>
+            <span>Tenant Age</span>
+            <span>{age}</span>
+          </div>
+          <div className="grid__box text-right">
+            <span>Est. Total</span>
+            <span>Market Size</span>
+            <span>{formatNumber(market_size)}</span>
+          </div>
+          <div className="grid__box text-right">
+            <span>Total Unique</span>
+            <span>Site Visitors</span>
+            <span>{formatNumber(usv)}</span>
+          </div>
+          <div className="grid__box text-right">
+            <span>Average</span>
+            <span>Market Growth</span>
+            <span>{formatPercent(growth, 1)}</span>
+          </div>
+          <div className="grid__box text-right">
+            <span>Est. Total {future_year}</span>
+            <span>Market Size</span>
+            <span>{formatNumber(future_size)}</span>
           </div>
         </div>
       </div>
