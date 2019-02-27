4456c85705ea7d217f38c931e84a3bf24384fd28
mingthegit <ming.dev1026@gmail.com>
2019-02-27 10:48:22 +0800

    added growth reach summary footer

--- src/js/components/market_size_growth_reach/index.js
+++ src/js/components/market_size_growth_reach/index.js
@@ -1,26 +1,69 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import './market_size_growth_reach.scss';
+import "./market_size_growth_reach.scss";
+import { formatNumber, formatPercent } from "../../utils/formatters";
 
 
-export function MarketSizeGrowthReach({ city }) {
+export function MarketSizeGrowthReach({
+  city,
+  future_year,
+  average: {
+    age,
+    growth,
+  },
+  total: {
+    market_size,
+    usv,
+    future_size,
+  }
+}) {
+
   return (
     <div className="market-size-growth-reach">
-      <div className="market-size-growth-reach__heading">
+      <div className="section__header">
         <p>Est. Market Size, Growth & Reach : {city}</p>
         <hr />
       </div>
 
-      <div className="market-size-growth-reach__content">
+      <div className="section__panel">
 
+        <div className="panel__summary">
+          <div className="summary__grid">
+            <div className="grid__box text-left">
+              <span>Average</span>
+              <span>Tenant Age</span>
+              <span>{age}</span>
+            </div>
+            <div className="grid__box text-right">
+              <span>Est. Total</span>
+              <span>Market Size</span>
+              <span>{formatNumber(market_size)}</span>
+            </div>
+            <div className="grid__box text-right">
+              <span>Total Unique</span>
+              <span>Site Visitors</span>
+              <span>{formatNumber(usv)}</span>
+            </div>
+            <div className="grid__box text-right">
+              <span>Average</span>
+              <span>Market Growth</span>
+              <span>{formatPercent(growth, 1)}</span>
+            </div>
+            <div className="grid__box text-right">
+              <span>Est. Total {future_year}</span>
+              <span>Market Size</span>
+              <span>{formatNumber(future_size)}</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
 MarketSizeGrowthReach.propTypes = {
-  city: PropTypes.string,
+  city: PropTypes.string.isRequired,
   market_sizes: PropTypes.arrayOf(
     PropTypes.shape({
       age_group: PropTypes.string,
@@ -29,17 +72,17 @@ MarketSizeGrowthReach.propTypes = {
       growth: PropTypes.number,
       future_size: PropTypes.number,
     })
-  ),
-  future_year: PropTypes.number,
+  ).isRequired,
+  future_year: PropTypes.number.isRequired,
   average: PropTypes.shape({
     age: PropTypes.number,
     growth: PropTypes.number,
-  }),
+  }).isRequired,
   total: PropTypes.shape({
     market_size: PropTypes.number,
     usv: PropTypes.number,
     future_size: PropTypes.number,
-  }),
+  }).isRequired,
 };
 
 export default MarketSizeGrowthReach;
