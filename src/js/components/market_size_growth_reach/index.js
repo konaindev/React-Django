45238b0d0d1bcaa730959a2ef8256f7344582465
mingthegit <ming.dev1026@gmail.com>
2019-02-27 09:31:55 +0800

    created MarketSizeGrowthReach component

---
+++ src/js/components/market_size_growth_reach/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import './market_size_growth_reach.css';
+
+
+export function MarketSizeGrowthReach({ city }) {
+  return (
+    <div className="market-size-growth-reach">
+      <div className="market-size-growth-reach__heading">
+        <p>Est. Market Size, Growth & Reach : {city}</p>
+        <hr />
+      </div>
+
+      <div className="market-size-growth-reach__content">
+        
+      </div>
+    </div>
+  );
+}
+
+MarketSizeGrowthReach.propTypes = {
+  city: PropTypes.string,
+  market_sizes: PropTypes.arrayOf(
+    PropTypes.shape({
+      age_group: PropTypes.string,
+      market_size: PropTypes.number,
+      usv: PropTypes.number,
+      growth: PropTypes.number,
+      future_size: PropTypes.number,
+    })
+  ),
+  future_year: PropTypes.number,
+  average: PropTypes.shape({
+    age: PropTypes.number,
+    growth: PropTypes.number,
+  }),
+  total: PropTypes.shape({
+    market_size: PropTypes.number,
+    usv: PropTypes.number,
+    future_size: PropTypes.number,
+  }),
+};
+
+export default MarketSizeGrowthReach;
