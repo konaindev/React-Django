807c6a887a2f88637e6338cb9f60f45ff3a7b9aa
Leonardo Courbassier <leonardo.courbassier@gmail.com>
2019-03-01 11:56:03 -0300

    Add TabNavigator component

--- src/js/components/container/index.js
+++ src/js/components/container/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
 
 import './container.scss';
 
-export const Container = ({ children }) => (
-  <div className="container">
+export const Container = ({ children, className }) => (
+  <div className={cn('container', className)}>
     {children}
   </div>
 );
+
+Container.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+};
 
 export default Container;
---
+++ src/js/components/tab_navigator/tab_navigator.stories.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { storiesOf } from '@storybook/react';
+import { withState } from '@dump247/storybook-state';
+
+import TabNavigator, { Tab } from './index';
+
+storiesOf('TabNavigator', module).add('default', withState({ selectedIndex: 0 })(({ store }) => (
+  <TabNavigator
+    selectedIndex={store.state.selectedIndex}
+    onChange={(index) => {
+      store.set({ selectedIndex: index });
+      action('tabChange')(index);
+    }}
+  >
+    <Tab label="Run Rate Model">
+      <div>Tab 1 Content here</div>
+      <div>This should not render unless it is the selected tab.</div>
+    </Tab>
+    <Tab label="Deadline-Driven Model">
+      <div>Tab 2 Content here</div>
+    </Tab>
+  </TabNavigator>
+)));
