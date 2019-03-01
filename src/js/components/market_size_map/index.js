d3e69a4e2bb35eb72e7bc61a05f7e7db7d5d060f
mingthegit <ming.dev1026@gmail.com>
2019-03-01 10:14:37 +0800

    night mode for google map

--- src/js/components/market_size_map/index.js
+++ src/js/components/market_size_map/index.js
@@ -1,15 +1,40 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import GoogleMap from 'google-map-react';
 
+import { stylesForNightMode } from './map_styles';
 import "./market_size_map.scss";
+
+const GOOGLE_MAP_API_KEY = 'AIzaSyBu4HU8t3rRXnfdkNjSV1_PIhzzrFFlVTs';
 
 
 export class MarketSizeMap extends Component {
 
+  onGoogleApiLoaded = ({ map, maps }) => {
+  }
+
   render() {
+    const {
+      center: {
+        coordinates: [ lat, lng ]
+      },
+      radius,
+      zip_codes
+    } = this.props;
+
+    const isCircleMode = zip_codes === undefined;
+
     return (
       <div className="market-size-map">
-        
+        <GoogleMap
+          bootstrapURLKeys={{ key: GOOGLE_MAP_API_KEY }}
+          center={{ lat, lng }}
+          zoom={12}
+          options={createMapOptions}
+          yesIWantToUseGoogleMapApiInternals={true}
+          onGoogleApiLoaded={this.onGoogleApiLoaded}
+        >
+        </GoogleMap>
       </div>
     );
   }
@@ -30,5 +55,12 @@ MarketSizeMap.propTypes = {
     })
   })
 };
+
+function createMapOptions(maps) {
+  return {
+    styles: stylesForNightMode
+  };
+}
+
 
 export default MarketSizeMap;
