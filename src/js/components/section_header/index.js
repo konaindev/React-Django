15a418996a5e9b36506a7fe1380e1dfa3ce7be93
Leonardo Courbassier <leonardo.courbassier@gmail.com>
2019-03-04 09:52:34 -0300

    Add SectionHeader component

---
+++ src/js/components/section_header/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./section_header.scss";
+
+export class SectionHeader extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    children: PropTypes.node
+  };
+
+  render() {
+    const { children, title } = this.props;
+    return (
+      <div className="section-header">
+        <h2 className="section-header__title">{title}</h2>
+        {children && <div className="section-header__extra">{this.props.children}</div>}
+      </div>
+    );
+  }
+}
+
+export default SectionHeader;
