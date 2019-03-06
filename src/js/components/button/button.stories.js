0d0533f02dcb79036fcb06ea46c6ad16d961e715
Leonardo Courbassier <leonardo.courbassier@gmail.com>
2019-03-06 17:35:15 -0300

    Add basic button component

---
+++ src/js/components/button/button.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { linkTo } from "@storybook/addon-links";
+
+import Button from "./index";
+
+storiesOf("Button", module)
+  .add("default", () => <Button>Default Button</Button>)
+  .add("selected", () => <Button selected>Selected Button</Button>);
