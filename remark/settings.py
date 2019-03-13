56292bcaca8e3418a2e6b7d0bde5da04fbf21876
Dave Peck <davepeck@gmail.com>
2019-03-13 15:26:43 -0700

    Enable google analytics.

--- remark/settings.py
+++ remark/settings.py
@@ -99,6 +99,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "remark.web.context_processors.google_analytics",
             ],
             "debug": DEBUG,
         },
