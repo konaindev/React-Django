52f1fae00ca1ccebbc4d9ec0332e9fc3c048bae4
Dave Peck <davepeck@gmail.com>
2019-03-18 16:06:28 -0700

    Remove dependency on pycel. Update importers/exporters.
    I also passed them through pyblack and made sure they were flake8-clean.

--- xls/exporters/tam-data.py
+++ xls/exporters/tam-data.py
@@ -5,18 +5,19 @@ import requests
 import time
 import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
-from openpyxl import load_workbook, Workbook
+from openpyxl import load_workbook
 
-FREE_MAP_TOOLS_URL = 'https://www.freemaptools.com/ajax/us/get-all-zip-codes-inside.php'
-FREE_MAP_REFERER = 'https://www.freemaptools.com/find-zip-codes-inside-radius.htm'
+FREE_MAP_TOOLS_URL = "https://www.freemaptools.com/ajax/us/get-all-zip-codes-inside.php"
+FREE_MAP_REFERER = "https://www.freemaptools.com/find-zip-codes-inside-radius.htm"
 
-STAT_ATLAS_AGE_URL = 'https://statisticalatlas.com/zip/%s/Age-and-Sex'
-STAT_ATLAS_HOUSEHOLD_URL = 'https://statisticalatlas.com/zip/%s/Household-Types'
-STAT_ATLAS_HOUSEHOLD_INCOME_URL = 'https://statisticalatlas.com/zip/%s/Household-Income'
-STAT_ATLAS_REFER = 'https://statisticalatlas.com/United-States/Overview'
+STAT_ATLAS_AGE_URL = "https://statisticalatlas.com/zip/%s/Age-and-Sex"
+STAT_ATLAS_HOUSEHOLD_URL = "https://statisticalatlas.com/zip/%s/Household-Types"
+STAT_ATLAS_HOUSEHOLD_INCOME_URL = "https://statisticalatlas.com/zip/%s/Household-Income"
+STAT_ATLAS_REFER = "https://statisticalatlas.com/United-States/Overview"
 
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36"
 MILES_KILOMETERS_RATIO = 1.60934
+
 
 def memoize(label):
     def outer_cache(func):
@@ -28,77 +29,75 @@ def memoize(label):
             filepath = "../../data/zipcode/%s" % filename
             if os.path.exists(filepath):
                 print("Read from disk: %s" % filepath)
-                file_ref = open(filepath, 'rb')
+                file_ref = open(filepath, "rb")
                 return pickle.load(file_ref)
             else:
                 print("Writing to disk: %s" % filepath)
                 result = func(*args)
-                file_ref = open(filepath, 'wb')
+                file_ref = open(filepath, "wb")
                 pickle.dump(result, file_ref)
                 time.sleep(20)
                 print("Finished writing.")
                 return result
+
         return inner_cache
+
     return outer_cache
 
-@memoize('fetch-zip-codes')
+
+@memoize("fetch-zip-codes")
 def fetch_zip_codes(latitude, longitude, radius):
     params = {
-        'radius' : radius,
-        'lat': latitude,
-        'lng': longitude,
-        'rn': 8192,
-        'showPOboxes': 'true'
+        "radius": radius,
+        "lat": latitude,
+        "lng": longitude,
+        "rn": 8192,
+        "showPOboxes": "true",
     }
-    headers = {
-        'user-agent': USER_AGENT,
-        'referer': FREE_MAP_REFERER
-    }
+    headers = {"user-agent": USER_AGENT, "referer": FREE_MAP_REFERER}
     response = requests.get(FREE_MAP_TOOLS_URL, params=params, headers=headers)
     root = ET.fromstring(response.text)
     result = []
     for child in root:
-        result.append(child.attrib['postcode'])
+        result.append(child.attrib["postcode"])
     return result
 
+
 def find_population(el):
-    return el.has_attr('title') and el['title'] == 'Population'
+    return el.has_attr("title") and el["title"] == "Population"
+
 
 def find_households(el):
-    return el.has_attr('title') and el['title'] == 'Households'
+    return el.has_attr("title") and el["title"] == "Households"
 
-@memoize('fetch-population')
+
+@memoize("fetch-population")
 def fetch_population(zipcode):
     url = STAT_ATLAS_AGE_URL % zipcode
-    headers = {
-        'user-agent': USER_AGENT,
-        'referer': STAT_ATLAS_REFER
-    }
+    headers = {"user-agent": USER_AGENT, "referer": STAT_ATLAS_REFER}
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.text, features="html.parser")
     pop_th = soup.find_all(find_population)[0]
     td_value = str(pop_th.td.text)
-    population = int(td_value.replace(',', ''))
+    population = int(td_value.replace(",", ""))
     print("Population: %d" % population)
 
     house_th = soup.find_all(find_households)[0]
     td_value = str(house_th.td.text)
-    households = int(td_value.replace(',', ''))
+    households = int(td_value.replace(",", ""))
     print("households: %d" % population)
 
     return (population, households)
 
+
 def fetch_svg(base_url, zipcode, figure_id):
     def find_figure(el):
-        if el.has_attr('id') and el['id'] == figure_id:
+        if el.has_attr("id") and el["id"] == figure_id:
             return True
         return False
 
     url = base_url % zipcode
-    headers = {
-        'user-agent': USER_AGENT,
-        'referer': STAT_ATLAS_REFER
-    }
+    headers = {"user-agent": USER_AGENT, "referer": STAT_ATLAS_REFER}
     response = requests.get(url, headers=headers)
 
     soup = BeautifulSoup(response.text, features="html.parser")
@@ -107,74 +106,76 @@ def fetch_svg(base_url, zipcode, figure_id):
         ref = 'id="%s"' % figure_id
         print("Searching for %s..." % ref)
         print("Results: %d" % (response.text.find(ref)))
-        raise Exception('Could not find the following figure: %s' % figure_id)
+        raise Exception("Could not find the following figure: %s" % figure_id)
 
     try:
-        svg = figures[0].find_all('svg')[0]
+        svg = figures[0].find_all("svg")[0]
     except Exception as e:
-        print('Exception: %s' % str(e))
+        print("Exception: %s" % str(e))
         print("Length: %d" % len(response.text))
-        print(figures[0].find('svg'))
+        print(figures[0].find("svg"))
         raise e
     return svg
 
-@memoize('fetch-age-segments')
+
+@memoize("fetch-age-segments")
 def fetch_age_segements_by_zip(zipcode):
-    svg = fetch_svg(STAT_ATLAS_AGE_URL, zipcode, 'figure/age-structure')
-    gs = svg.g.find_all('g')
+    svg = fetch_svg(STAT_ATLAS_AGE_URL, zipcode, "figure/age-structure")
+    gs = svg.g.find_all("g")
     result = []
     for x in range(len(gs)):
-        #print("%d: %s" % (x, gs[x]))
+        # print("%d: %s" % (x, gs[x]))
         if x >= 26 and x < 49:
             txt = gs[x].title.text
-            value = float(txt.replace('%', ''))
+            value = float(txt.replace("%", ""))
             result.append(value / 100.0)
     # print(result)
     return result
 
-@memoize('fetch-household-type')
+
+@memoize("fetch-household-type")
 def fetch_household_type(zipcode):
-    svg = fetch_svg(STAT_ATLAS_HOUSEHOLD_URL, zipcode, 'figure/household-types')
-    gs = svg.g.find_all('g')
+    svg = fetch_svg(STAT_ATLAS_HOUSEHOLD_URL, zipcode, "figure/household-types")
+    gs = svg.g.find_all("g")
     result = []
     for x in range(len(gs)):
-        #print("%d: %s" % (x, gs[x]))
+        # print("%d: %s" % (x, gs[x]))
         if x >= 7:
             txt = gs[x].title.text
-            value = float(txt.replace('%', ''))
+            value = float(txt.replace("%", ""))
             result.append(value / 100.0)
     return result
 
-@memoize('fetch-household-income')
+
+@memoize("fetch-household-income")
 def fetch_household_income(zipcode):
-    svg = fetch_svg(STAT_ATLAS_HOUSEHOLD_INCOME_URL, zipcode, 'figure/household-income-percentiles')
-    gs = svg.g.find_all('g')
-    result = [
-        gs[8],
-        gs[10],
-        gs[12],
-        gs[14],
-        gs[16],
-        gs[18]
-    ]
+    svg = fetch_svg(
+        STAT_ATLAS_HOUSEHOLD_INCOME_URL, zipcode, "figure/household-income-percentiles"
+    )
+    gs = svg.g.find_all("g")
+    result = [gs[8], gs[10], gs[12], gs[14], gs[16], gs[18]]
     for x in range(len(result)):
         txt = result[x].title.text
-        result[x] = float(txt.replace('$', '').replace(',', ''))
+        result[x] = float(txt.replace("$", "").replace(",", ""))
     return result
 
-@memoize('fetch-household-income-dist')
+
+@memoize("fetch-household-income-dist")
 def fetch_household_income_distribution(zipcode):
-    svg = fetch_svg(STAT_ATLAS_HOUSEHOLD_INCOME_URL, zipcode, 'figure/household-income-distribution')
+    svg = fetch_svg(
+        STAT_ATLAS_HOUSEHOLD_INCOME_URL, zipcode, "figure/household-income-distribution"
+    )
     print(svg)
-    gs = svg.g.find_all('g')
+    gs = svg.g.find_all("g")
     result = []
     for x in range(len(gs)):
         if x >= 19 and x < 35:
             txt = gs[x].title.text
-            value = float(txt.replace('%', ''))
+            value = float(txt.replace("%", ""))
             result.append(value / 100.0)
     print(result)
     return result
+
 
 def write_labeled_data(ws, title, labels, data, start_row):
     ws.cell(column=1, row=start_row, value=title)
@@ -184,9 +185,11 @@ def write_labeled_data(ws, title, labels, data, start_row):
         ws.cell(column=2, row=actual_row, value=data[x])
     return None
 
+
 def write_label_item(ws, label, item, row):
     ws.cell(column=1, row=row, value=label)
     ws.cell(column=2, row=row, value=item)
+
 
 AGE_SEGMENT_LABELS = [
     "85+",
@@ -211,15 +214,15 @@ AGE_SEGMENT_LABELS = [
     "15-17",
     "10-14",
     "5-9",
-    "0-4"
+    "0-4",
 ]
 
 HOUSEHOLD_TYPE_LABELS = [
-    'Married',
-    'Single Female',
-    'Single Male',
-    'One Person',
-    'Non-Family'
+    "Married",
+    "Single Female",
+    "Single Male",
+    "One Person",
+    "Non-Family",
 ]
 
 INCOME_DIST_LABELS = [
@@ -238,10 +241,11 @@ INCOME_DIST_LABELS = [
     "$20-25k",
     "$15-20k",
     "$10-15k",
-    "< $10k"
+    "< $10k",
 ]
 
 ZIP_DATA_SHEET_NAME = "Zip Data %s"
+
 
 def fill_zip_worksheet(workbook, zipcode):
     # Fetch all data first - if a Zip Code doesn't work. Ignore it.
@@ -256,11 +260,25 @@ def fill_zip_worksheet(workbook, zipcode):
 
     # Create speadsheet
     worksheet = workbook.create_sheet(title=ZIP_DATA_SHEET_NAME % zipcode)
-    write_label_item(worksheet, 'Total Population', population[0], 1)
-    write_label_item(worksheet, 'Number of Households', population[1], 2)
-    write_labeled_data(worksheet, 'Population by Age Segment', AGE_SEGMENT_LABELS, age_segments, 4)
-    write_labeled_data(worksheet, 'Household by Type', HOUSEHOLD_TYPE_LABELS, household_type, len(age_segments) + 6)
-    write_labeled_data(worksheet, 'Income Distribution', INCOME_DIST_LABELS, income_dist, len(age_segments) + len(household_type) + 8)
+    write_label_item(worksheet, "Total Population", population[0], 1)
+    write_label_item(worksheet, "Number of Households", population[1], 2)
+    write_labeled_data(
+        worksheet, "Population by Age Segment", AGE_SEGMENT_LABELS, age_segments, 4
+    )
+    write_labeled_data(
+        worksheet,
+        "Household by Type",
+        HOUSEHOLD_TYPE_LABELS,
+        household_type,
+        len(age_segments) + 6,
+    )
+    write_labeled_data(
+        worksheet,
+        "Income Distribution",
+        INCOME_DIST_LABELS,
+        income_dist,
+        len(age_segments) + len(household_type) + 8,
+    )
 
 
 def pop_formula(zipcodes, percent_values):
@@ -292,18 +310,19 @@ INCOME_DIST_LEVELS = (
     (20000, 49),
     (15000, 50),
     (10000, 51),
-    (0, 52)
+    (0, 52),
 )
+
 
 def fill_computation_tab(worksheet, zipcodes):
     # Add ACS Population Reference Sumed across all zipcodes
     age_group_pop_schema = (
-        (3, ('23', '22', '21', '20')),# 18-24
-        (4, ('18', '19')),# 25-34
-        (5, ('17', '16')),# 35-44
-        (6, ('15', '14')),# 45-54
-        (7, ('13', '12', '11')),# 55-64
-        (8, ('10', '9', '8', '7', '6', '5')),# 65+
+        (3, ("23", "22", "21", "20")),  # 18-24
+        (4, ("18", "19")),  # 25-34
+        (5, ("17", "16")),  # 35-44
+        (6, ("15", "14")),  # 45-54
+        (7, ("13", "12", "11")),  # 55-64
+        (8, ("10", "9", "8", "7", "6", "5")),  # 65+
     )
     for dest_row, percent_values in age_group_pop_schema:
         formula = pop_formula(zipcodes, percent_values)
@@ -317,7 +336,10 @@ def fill_computation_tab(worksheet, zipcodes):
         for x in range(start_x, len(INCOME_DIST_LEVELS)):
             for zip in zipcodes:
                 tab_name = ZIP_DATA_SHEET_NAME % zip
-                numerator.append("('%s'!B%d*'%s'!B1)" % (tab_name, INCOME_DIST_LEVELS[x][1], tab_name))
+                numerator.append(
+                    "('%s'!B%d*'%s'!B1)"
+                    % (tab_name, INCOME_DIST_LEVELS[x][1], tab_name)
+                )
 
             denominator.append("'%s'!B1" % tab_name)
 
@@ -328,8 +350,8 @@ def fill_computation_tab(worksheet, zipcodes):
         cell_formulas.append(final_formula)
 
     for x in range(3):
-        worksheet.cell(column=1, row=29+x, value=INCOME_GROUPS[x])
-        worksheet.cell(column=2, row=29+x, value=cell_formulas[x])
+        worksheet.cell(column=1, row=29 + x, value=INCOME_GROUPS[x])
+        worksheet.cell(column=2, row=29 + x, value=cell_formulas[x])
 
     # Total population
     formula = []
@@ -342,15 +364,15 @@ def fill_computation_tab(worksheet, zipcodes):
     numerator = []
     for zip in zipcodes:
         tab_name = ZIP_DATA_SHEET_NAME % zip
-        numerator.append("(('%s'!B%d+'%s'!B%d+'%s'!B%d)*'%s'!B1)" % (tab_name, 30, tab_name, 31, tab_name, 32, tab_name))
+        numerator.append(
+            "(('%s'!B%d+'%s'!B%d+'%s'!B%d)*'%s'!B1)"
+            % (tab_name, 30, tab_name, 31, tab_name, 32, tab_name)
+        )
     final_formula = "=(%s)/'Computation'!B38" % "+".join(numerator)
     worksheet.cell(column=2, row=34, value=final_formula)
 
 
-
-ZIP_CODES = [
-    85013
-]
+ZIP_CODES = [85013]
 
 # Meridian
 # Zip Codes: 84101,84111,84102,84112,84115,84105
@@ -360,17 +382,16 @@ ZIP_CODES = [
 # Zip Codes: 85013
 # Income Groups: 25000, 45000, 60000
 
-INCOME_GROUPS = [
-    25000, 45000, 60000
-]
+INCOME_GROUPS = [25000, 45000, 60000]
+
 
 def main():
-    latitude = sys.argv[1]
-    longitude = sys.argv[2]
-    radius = float(sys.argv[3])
+    # latitude = sys.argv[1]
+    # longitude = sys.argv[2]
+    # radius = float(sys.argv[3])
 
-    zipcodes = ZIP_CODES # fetch_zip_codes(latitude, longitude, radius)
-    workbook = load_workbook('../templates/tam-template.xlsx')
+    zipcodes = ZIP_CODES  # fetch_zip_codes(latitude, longitude, radius)
+    workbook = load_workbook("../templates/tam-template.xlsx")
 
     # generate Zip Code Worksheet
     for zipcode in zipcodes:
@@ -382,12 +403,13 @@ def main():
             print("ZipCode Failed: %s" % zipcode)
             raise e
 
-    fill_computation_tab(workbook['Computation'], zipcodes)
+    fill_computation_tab(workbook["Computation"], zipcodes)
 
     # delete sample data tab
-    workbook.remove(workbook['Sample Zip Data'])
+    workbook.remove(workbook["Sample Zip Data"])
 
     workbook.save(filename="../../dist/remarkably-tam-test.xlsx")
+
 
 # fetch_zip_codes(latitude, longitude, radius * MILES_KILOMETERS_RATIO)
 # fetch_age_segements_by_zip('85013')
