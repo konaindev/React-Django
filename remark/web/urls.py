bac89d3d900e8eea66e301f5a8d70e0304e732c7
Leonardo Courbassier <leonardo.courbassier@gmail.com>
2019-03-18 11:51:12 -0300

    Make Client Login the Homepage

--- remark/web/urls.py
+++ remark/web/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 
 from django.urls import path
 from django.views.generic.base import TemplateView
+from django.contrib.auth import views as auth_views
+        
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="web/home.html"), name="home")
+    path(
+        "",
+        auth_views.LoginView.as_view(template_name="users/login.html"),
+        name="home-login",
+    ),
 ]
