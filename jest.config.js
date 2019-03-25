9d84c11599158b48974d8f4dcc6f946c564b09d3
mingthegit <ming.dev1026@gmail.com>
2019-03-25 22:25:33 +0800

    added jest mocks for styles and files

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,9 @@ module.exports = {
   testMatch: [
     "<rootDir>/src/js/**/__tests__/**/*.[jt]s?(x)",
     "<rootDir>/src/js/**/?(*.)+(spec|test).[jt]s?(x)"
-  ]
+  ],
+  "moduleNameMapper": {
+    "\\.(jpg|jpeg|png|gif|eot|otf|svg|ttf|woff|woff2)$": "<rootDir>/src/js/__mocks__/fileMock.js",
+    "\\.(css|scss)$": "<rootDir>/src/js/__mocks__/styleMock.js"
+  }
 };
