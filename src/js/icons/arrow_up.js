270b4c4934e8de22b07d3908f8728b36bfd012d7
Leonardo Courbassier <leonardo.courbassier@gmail.com>
2019-03-28 13:00:20 -0300

    Fix Performance Report Styling issues - Add IconBase component to use svg icon as react component - Add stories for icon components

---
+++ src/js/icons/arrow_up.js
@@ -0,0 +1,10 @@
+import React from "react";
+import IconBase from "./icon_base";
+
+const ArrowUp = props => (
+  <IconBase viewBox="0 0 10 5" {...props}>
+    <path d="M5 0l5 5H0z" fill="currentColor" fill-rule="evenodd" />
+  </IconBase>
+);
+
+export default ArrowUp;
---
+++ src/js/icons/index.js
@@ -0,0 +1,3 @@
+export { default as ArrowDown } from "./arrow_down";
+export { default as ArrowSide } from "./arrow_side";
+export { default as ArrowUp } from "./arrow_up";
