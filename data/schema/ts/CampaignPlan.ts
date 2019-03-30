dcc9885f00ca908257c7752bbb46f5e7fea37971
Dave Peck <davepeck@gmail.com>
2019-03-29 17:08:14 -0700

    Skeleton out campaign categories

--- data/schema/ts/CampaignPlan.ts
+++ data/schema/ts/CampaignPlan.ts
@@ -43,6 +43,80 @@ interface Overview {
   target_investment: InvestmentExpenses;
 }
 
+/** Allowable schedule statuses */
+enum ScheduleStatus {
+  not_started = "not_started",
+  in_progress = "in_progress",
+  complete = "complete"
+}
+
+/** Known cost categories */
+enum CostCategory {
+  one_time = "one_time",
+  monthly = "monthly"
+}
+
+interface Tactic {
+  /** The name of the tactic, ('Brand Strategy') */
+  name: string;
+
+  /** Details about the tactic (shown in tooltip, typically) */
+  tooltip: string | null;
+
+  /** The scheduled date */
+  schedule: t.date;
+
+  /** The current schedule status */
+  status: ScheduleStatus;
+
+  /** Notes about the tactic (arbitrary markdown) */
+  notes: t.markdown | null;
+
+  /** The base cost for the tactic */
+  base_cost: t.currency;
+
+  /** The cost category */
+  cost_category: CostCategory;
+
+  /** Total cost @computed */
+  total_cost: t.currency;
+}
+
+interface DemandCreationTargets<T> {
+  /** Unique site visitors */
+  usv: T;
+
+  /** Inquiries */
+  inq: T;
+}
+
+interface DemandCreationTactic extends Tactic {
+  /** Campaign plan target volumes for demand creation */
+  volumes: DemandCreationTargets<t.integer>;
+
+  /** Campaign plan target costs for demand creation */
+  costs: DemandCreationTargets<t.currency>;
+}
+
+interface CampaignCategory {
+  /** List of tactics for this category */
+  tactics: Tactic[];
+}
+
+interface ReputationBuilding extends CampaignCategory {}
+
+interface DemandCreation extends CampaignCategory {
+  tactics: DemandCreationTactic[];
+}
+
+interface LeasingEnablement extends CampaignCategory {}
+
+interface MarketIntelligence extends CampaignCategory {}
+
 export interface CampaignPlan {
   overview: Overview;
+  reputation_building: ReputationBuilding;
+  demand_creation: DemandCreation;
+  leasing_enablement: LeasingEnablement;
+  market_intelligence: MarketIntelligence;
 }
