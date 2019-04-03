769447751d2321d0f9ac21485ed4d9536cfe768f
mingthegit <ming.dev1026@gmail.com>
2019-04-03 16:24:31 -0400

    fixed timezone issue while converting raw string

--- src/js/utils/formatters.js
+++ src/js/utils/formatters.js
@@ -141,7 +141,7 @@ export const formatDate = (value, year = true) => {
  * formatDateWithTokens("2018-12-17", "MMM D, YYYY"); // Dec 17, 2018
  */
 export const formatDateWithTokens = (v, tokens) => {
-  return dateFnformat(new Date(v), tokens);
+  return dateFnformat(v, tokens);
 };
 
 /**
