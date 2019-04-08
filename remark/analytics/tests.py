2a835fbad074b6e4a7744913f563d337ad4f46b6
Leonardo Courbassier <leonardo.courbassier@gmail.com>
2019-04-08 15:47:16 -0300

    Added basic tests for `get_report_usv_age_from_response`

--- remark/analytics/tests.py
+++ remark/analytics/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .google_analytics import get_report_usv_age_from_response
+
+
+class GoogleAnlayticsTest(TestCase):
+    """
+    Test that all computed properties on a default Period instance
+    return sane values.
+    """
+
+    def setUp(self):
+        self.response_mock = {  
+            "reports":[{
+                "data":{  
+                    "rows":[{  
+                        "dimensions":[  
+                            "25-34"
+                        ],
+                        "metrics":[{  
+                            "values":[  
+                                "18"
+                            ]
+                        }]
+                    }]
+                }
+            }]
+        }
+
+    def test_get_report_usv_age_from_response(self):
+        result = get_report_usv_age_from_response(self.response_mock)
+        self.assertEqual(result, [0, 18, 0, 0, 0, 0])
