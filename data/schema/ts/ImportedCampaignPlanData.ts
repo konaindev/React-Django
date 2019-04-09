691aeec0e74cceb876e100e69a0d6858cb0fce09
Dave Peck <davepeck@gmail.com>
2019-04-09 11:33:00 -0700

    Define schema for imported campaign plan data.

---
+++ data/schema/ts/ImportedCampaignPlanData.ts
@@ -0,0 +1,4 @@
+import { CampaignPlan } from "./CampaignPlan";
+
+/** For now, imported campaign plan data is equivalent to live campaign plan data. */
+export interface ImportedCampaignPlanData extends CampaignPlan {}
