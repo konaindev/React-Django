3af32ff042ef9cb0ffe6138bb3e695ee608ef92d
mingthegit <ming.dev1026@gmail.com>
2019-04-09 23:56:56 +0800

    nothing changes, cosmetic change to babel config

--- jest.config.js
+++ jest.config.js
@@ -5,8 +5,9 @@ module.exports = {
     "<rootDir>/src/js/**/__tests__/**/*.[jt]s?(x)",
     "<rootDir>/src/js/**/?(*.)+(spec|test).[jt]s?(x)"
   ],
-  "moduleNameMapper": {
-    "\\.(jpg|jpeg|png|gif|eot|otf|svg|ttf|woff|woff2)$": "<rootDir>/src/js/__mocks__/fileMock.js",
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|svg|ttf|woff|woff2)$":
+      "<rootDir>/src/js/__mocks__/fileMock.js",
     "\\.(css|scss)$": "<rootDir>/src/js/__mocks__/styleMock.js"
   }
 };
