45e66d9481b5d6a7655d0ca556b65be9b36a5576
Leonardo Courbassier <leonardo.courbassier@gmail.com>
2019-04-24 13:52:20 -0300

    Implement address geocoding on save
    * Add latitude & longitude properties
    
    * Throw errors when address components does not exist in the response
    
    * Add tests for process_geocode_response
    
    * Concatenate street_number & route components for street_address_1

--- remark/geo/models.py
+++ remark/geo/models.py
@@ -39,3 +39,105 @@ class Address(models.Model):
     geo_code_json = JSONField(
         help_text="Google Geo Code JSON"
     )
+
+    @property
+    def latitude(self):
+        try:
+            return self.geo_code_json["geometry"]["location"]["lat"]
+        except:
+            return None
+
+    @property
+    def longitude(self):
+        try:
+            return self.geo_code_json["geometry"]["location"]["lng"]
+        except:
+            return None
+
+    def save(self, *args, **kwargs):
+        full_address = ", ".join(
+            [item for item in [
+                self.street_address_1,
+                self.street_address_2,
+                self.city,
+                self.state,
+                self.zip_code,
+                self.country
+            ] if item is not None and item != ""]
+        )
+
+        gmaps = googlemaps.Client(key=os.environ.get("GOOGLE_GEOCODE_API_KEY"))
+        geocode_result = gmaps.geocode(full_address)
+        self.process_geocode_response(geocode_result)
+        return super().save(*args, **kwargs)
+
+    def process_geocode_response(self, geocode_result):
+        try:
+            address_components = geocode_result[0]["address_components"]
+        except:
+            raise ValidationError("Address components not found")
+
+        street_number_matches = [
+            item["short_name"] for item in address_components \
+            if "street_number" in item["types"]
+        ]
+        route_matches = [
+            item["short_name"] for item in address_components \
+            if "route" in item["types"]
+        ]
+
+        street_address_1_lines = street_number_matches + route_matches
+        if len(street_address_1_lines) > 0:
+            self.street_address_1 = " ".join(street_address_1_lines)
+        else:
+            raise ValidationError("Street address 1 not found")
+
+        self.street_address_2 = None
+
+        city_matches = [
+            item["short_name"] for item in address_components \
+            if "administrative_area_level_2" in item["types"]
+        ]
+        if len(city_matches) > 0:
+            self.city = city_matches[0]
+        else:
+            raise ValidationError("City not found")
+
+        state_matches = [
+            item["short_name"] for item in address_components \
+            if "administrative_area_level_1" in item["types"]
+        ]
+        if len(state_matches) > 0:
+            self.state = state_matches[0]
+        else:
+            raise ValidationError("State not found")
+
+        country_matches = [
+            item["short_name"] for item in address_components \
+            if "country" in item["types"]
+        ]
+        if len(country_matches) > 0:
+            self.country = country_matches[0]
+        else:
+            raise ValidationError("Country not found")
+
+        zip_code_matches = [
+            item["short_name"] for item in address_components \
+            if "postal_code" in item["types"]
+        ]
+        if len(zip_code_matches) > 0:
+            self.zip_code = zip_code_matches[0]
+        else:
+            raise ValidationError("Zipcode not found")
+
+        try:
+            lat = geocode_result[0]["geometry"]["location"]["lat"]
+        except:
+            raise ValidationError("Latitude not found")
+
+        try:
+            lng = geocode_result[0]["geometry"]["location"]["lng"]
+        except:
+            raise ValidationError("Longitude not found")
+
+        self.geo_code_json = geocode_result
--- remark/geo/tests.py
+++ remark/geo/tests.py
@@ -1,1 +1,95 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+from copy import copy
+
+from .models import Address
+
+
+class AddressModelTest(TestCase):
+    """
+    Test that all computed properties on a default Period instance
+    return sane values.
+    """
+
+    def setUp(self):
+        self.geocode_result = [
+            {
+                "address_components" : [
+                    {
+                        "long_name" : "1600",
+                        "short_name" : "1600",
+                        "types" : [ "street_number" ]
+                    },
+                    {
+                        "long_name" : "Amphitheatre Pkwy",
+                        "short_name" : "Amphitheatre Pkwy",
+                        "types" : [ "route" ]
+                    },
+                    {
+                        "long_name" : "Mountain View",
+                        "short_name" : "Mountain View",
+                        "types" : [ "locality", "political" ]
+                    },
+                    {
+                        "long_name" : "Santa Clara County",
+                        "short_name" : "Santa Clara County",
+                        "types" : [ "administrative_area_level_2", "political" ]
+                    },
+                    {
+                        "long_name" : "California",
+                        "short_name" : "CA",
+                        "types" : [ "administrative_area_level_1", "political" ]
+                    },
+                    {
+                        "long_name" : "United States",
+                        "short_name" : "US",
+                        "types" : [ "country", "political" ]
+                    },
+                    {
+                        "long_name" : "94043",
+                        "short_name" : "94043",
+                        "types" : [ "postal_code" ]
+                    }
+                ],
+                "formatted_address" : "1600 Amphitheatre Parkway, Mountain View, CA 94043, USA",
+                "geometry" : {
+                    "location" : {
+                        "lat" : 37.4224764,
+                        "lng" : -122.0842499
+                    },
+                    "location_type" : "ROOFTOP",
+                    "viewport" : {
+                        "northeast" : {
+                            "lat" : 37.4238253802915,
+                            "lng" : -122.0829009197085
+                        },
+                        "southwest" : {
+                            "lat" : 37.4211274197085,
+                            "lng" : -122.0855988802915
+                        }
+                    }
+                },
+                "place_id" : "ChIJ2eUgeAK6j4ARbn5u_wAGqWA",
+                "types" : [ "street_address" ]
+            }
+        ]
+
+    def test_process_geocode_response_with_correct_response(self):
+        address = Address()
+        address.process_geocode_response(self.geocode_result)
+        self.assertEqual(address.street_address_1, "1600 Amphitheatre Pkwy")
+        self.assertEqual(address.street_address_2, None)
+        self.assertEqual(address.city, "Santa Clara County")
+        self.assertEqual(address.state, "CA")
+        self.assertEqual(address.country, "US")
+
+    def test_process_geocode_response_with_missing_street_address_1(self):
+        mal_geocode_result = copy(self.geocode_result)
+        # street addres 1 is omitted
+        mal_geocode_result[0]["address_components"] = self.geocode_result[0]["address_components"][2:]
+        address = Address()
+        self.assertRaises(
+            ValidationError,
+            address.process_geocode_response,
+            mal_geocode_result
+        )
