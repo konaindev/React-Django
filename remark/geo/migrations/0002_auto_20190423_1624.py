3e52e21ad5874ef163155a4b5f2b7b23a525afa4
Leonardo Courbassier <leonardo.courbassier@gmail.com>
2019-04-25 12:57:02 -0300

    Fix migration name issue

--- remark/geo/migrations/0002_auto_20190423_1624.py
+++ remark/geo/migrations/0002_auto_20190423_1624.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('geo', '0002_address'),
+        ('geo', '0001_initial'),
     ]
 
     operations = [
