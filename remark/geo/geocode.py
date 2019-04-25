f78827a9d65437135b3f80f2581f85ae1b514eb2
Dave Peck <davepeck@gmail.com>
2019-04-25 16:40:13 -0700

    Break out geocode code into helper library

---
+++ remark/geo/geocode.py
@@ -0,0 +1,143 @@
+"""
+A simple wrapper around the google geocode APIs.
+"""
+from django.conf import settings
+
+import googlemaps
+
+from remark.lib.logging import getLogger
+
+logger = getLogger(__name__)
+
+
+class GeocodeError(Exception):
+    pass
+
+
+class GeocodeResult:
+    """
+    Wrapper around a raw JSON blob returned by the google Geocoding API.
+
+    Provides convenience accessors to the data we want.
+    """
+
+    def __init__(self, geocode_json):
+        if not isinstance(geocode_json, list):
+            raise GeocodeError(f"Got a geocode_json that wasn't a list: {geocode_json}")
+        if not geocode_json:
+            raise GeocodeError(f"Got an empty geocode response.")
+        self.geocode_json = geocode_json
+        self.first_result = geocode_json[0]
+
+    def get_address_component(self, type_):
+        """
+        Returns the (long_name, short_name) for the first address component
+        matching the requested type_. If no components match, return (None, None)
+        """
+        for component in self.first_result.get("address_components", []):
+            if type_ in component.get("types", []):
+                return (component.get("long_name"), component.get("short_name"))
+        return (None, None)
+
+    def get_long_component(self, type_):
+        """
+        Returns the long name for the first address component matching
+        the requested type_. If no components match, return None.
+        """
+        return self.get_address_component(type_)[0]
+
+    def get_short_component(self, type_):
+        """
+        Returns the short name for the first address component matching
+        the requested type_. If no components match, return None.
+        """
+        return self.get_address_component(type_)[1]
+
+    @property
+    def latitude(self):
+        """Return the geocoded latitude, or None."""
+        return self.first_result.get("geometry", {}).get("location", {}).get("lat")
+
+    @property
+    def longitude(self):
+        """Return the geocoded longitude, or None."""
+        return self.first_result.get("geometry", {}).get("location", {}).get("lng")
+
+    @property
+    def formatted_address(self):
+        """Return the canonical formatted address, if available."""
+        return self.first_result.get("formatted_address")
+
+    @property
+    def street_address_1(self):
+        """Return the street, if known. (ex: 2901 NE Blakeley Street)"""
+        number = self.get_short_component("street_number") or ""
+        route = self.get_short_component("route") or ""
+        return f"{number} {route}".strip() or None
+
+    @property
+    def street_address_2(self):
+        """This appears to have no analogue in google geocoding. Return None."""
+        return None
+
+    @property
+    def city(self):
+        """Return the full city name, if known. (Seattle)"""
+        maybe_city = self.get_long_component("locality")
+        if not maybe_city:
+            maybe_city = self.get_long_component("postal_town")
+        if not maybe_city:
+            maybe_city = self.get_long_component("sublocality")
+        return maybe_city or None
+
+    @property
+    def state(self):
+        """Return the short state name, if known. (WA)"""
+        return self.get_short_component("administrative_area_level_1")
+
+    @property
+    def country(self):
+        """Return the short country name, if known. (US)"""
+        return self.get_short_component("country")
+
+    @property
+    def postal_code(self):
+        """Return the postal code, if available."""
+        return self.get_long_component("postal_code")
+
+    @property
+    def zip5(self):
+        """Return an explicit 5-digit zipcode, if available."""
+        maybe_postal = self.postal_code or ""
+        return maybe_postal[:5] or None
+
+    @property
+    def is_complete(self):
+        """Return True if all basic address components are availale."""
+        components = [
+            self.latitude,
+            self.longitude,
+            self.street_address_1,
+            self.city,
+            self.state,
+            self.country,
+            self.postal_code,
+        ]
+        return all([bool(component) for component in components])
+
+
+def geocode(location):
+    """
+    Given an arbitrary location description (like 'University Of Washington'
+    or '1600 Pennsylvania Ave'), attempt to geocode the location.
+
+    On error, return None. On success, return a GeocodeResult.
+    """
+    try:
+        client = googlemaps.Client(key=settings.GOOGLE_GEOCODE_API_KEY)
+        geocode_json = client.geocode(location)
+        result = GeocodeResult(geocode_json)
+    except Exception as e:
+        logger.error(f"Unable to geocode '{location}': {e}")
+        result = None
+    return result
---
+++ remark/geo/test_geocode.py
@@ -0,0 +1,225 @@
+from django.test import TestCase
+
+
+from .geocode import GeocodeResult
+
+
+class TestCompleteGeocodeResult(TestCase):
+    # geocode("University Zoka Coffee, Seattle")
+    TEST_GEOCODE_JSON = [
+        {
+            "address_components": [
+                {"long_name": "2901", "short_name": "2901", "types": ["street_number"]},
+                {
+                    "long_name": "Northeast Blakeley Street",
+                    "short_name": "NE Blakeley St",
+                    "types": ["route"],
+                },
+                {
+                    "long_name": "Northeast Seattle",
+                    "short_name": "Northeast Seattle",
+                    "types": ["neighborhood", "political"],
+                },
+                {
+                    "long_name": "Seattle",
+                    "short_name": "Seattle",
+                    "types": ["locality", "political"],
+                },
+                {
+                    "long_name": "King County",
+                    "short_name": "King County",
+                    "types": ["administrative_area_level_2", "political"],
+                },
+                {
+                    "long_name": "Washington",
+                    "short_name": "WA",
+                    "types": ["administrative_area_level_1", "political"],
+                },
+                {
+                    "long_name": "United States",
+                    "short_name": "US",
+                    "types": ["country", "political"],
+                },
+                {"long_name": "98105", "short_name": "98105", "types": ["postal_code"]},
+            ],
+            "formatted_address": "2901 NE Blakeley St, Seattle, WA 98105, USA",
+            "geometry": {
+                "location": {"lat": 47.666069, "lng": -122.297606},
+                "location_type": "ROOFTOP",
+                "viewport": {
+                    "northeast": {"lat": 47.6674179802915, "lng": -122.2962570197085},
+                    "southwest": {"lat": 47.6647200197085, "lng": -122.2989549802915},
+                },
+            },
+            "place_id": "ChIJXfLA8YYUkFQR6Zimch60wH0",
+            "plus_code": {
+                "compound_code": "MP82+CX Seattle, Washington, United States",
+                "global_code": "84VVMP82+CX",
+            },
+            "types": ["cafe", "establishment", "food", "point_of_interest", "store"],
+        },
+        {
+            "address_components": [
+                {"long_name": "1220", "short_name": "1220", "types": ["street_number"]},
+                {
+                    "long_name": "West Nickerson Street",
+                    "short_name": "W Nickerson St",
+                    "types": ["route"],
+                },
+                {
+                    "long_name": "Seattle",
+                    "short_name": "Seattle",
+                    "types": ["locality", "political"],
+                },
+                {
+                    "long_name": "Washington",
+                    "short_name": "WA",
+                    "types": ["administrative_area_level_1", "political"],
+                },
+                {
+                    "long_name": "United States",
+                    "short_name": "US",
+                    "types": ["country", "political"],
+                },
+                {"long_name": "98119", "short_name": "98119", "types": ["postal_code"]},
+            ],
+            "formatted_address": "1325, 1220 W Nickerson St, Seattle, WA 98119, United States",
+            "geometry": {
+                "location": {"lat": 47.6558453, "lng": -122.373121},
+                "location_type": "ROOFTOP",
+                "viewport": {
+                    "northeast": {"lat": 47.65719428029149, "lng": -122.3717720197085},
+                    "southwest": {"lat": 47.6544963197085, "lng": -122.3744699802915},
+                },
+            },
+            "place_id": "ChIJn5yWtKQVkFQRIY0mu9deQLo",
+            "plus_code": {
+                "compound_code": "MJ4G+8Q Seattle, Washington, United States",
+                "global_code": "84VVMJ4G+8Q",
+            },
+            "types": ["establishment", "food", "point_of_interest"],
+        },
+    ]
+
+    def setUp(self):
+        super().setUp()
+        self.result = GeocodeResult(self.TEST_GEOCODE_JSON)
+
+    def test_latitude(self):
+        self.assertEqual(self.result.latitude, 47.666069)
+
+    def test_longitude(self):
+        self.assertEqual(self.result.longitude, -122.297606)
+
+    def test_formatted_address(self):
+        self.assertEqual(
+            self.result.formatted_address, "2901 NE Blakeley St, Seattle, WA 98105, USA"
+        )
+
+    def test_street_address_1(self):
+        self.assertEqual(self.result.street_address_1, "2901 NE Blakeley St")
+
+    def test_street_address_2(self):
+        self.assertEqual(self.result.street_address_2, None)
+
+    def test_city(self):
+        self.assertEqual(self.result.city, "Seattle")
+
+    def test_state(self):
+        self.assertEqual(self.result.state, "WA")
+
+    def test_country(self):
+        self.assertEqual(self.result.country, "US")
+
+    def test_postal_code(self):
+        self.assertEqual(self.result.postal_code, "98105")
+
+    def test_zip5(self):
+        self.assertEqual(self.result.zip5, "98105")
+
+    def test_is_complete(self):
+        self.assertTrue(self.result.is_complete)
+
+
+class TestIncompleteGeocodeResult(TestCase):
+    # geocode("University Of Washington in Seattle")
+    TEST_GEOCODE_JSON = [
+        {
+            "address_components": [
+                {
+                    "long_name": "Seattle",
+                    "short_name": "Seattle",
+                    "types": ["locality", "political"],
+                },
+                {
+                    "long_name": "King County",
+                    "short_name": "King County",
+                    "types": ["administrative_area_level_2", "political"],
+                },
+                {
+                    "long_name": "Washington",
+                    "short_name": "WA",
+                    "types": ["administrative_area_level_1", "political"],
+                },
+                {
+                    "long_name": "United States",
+                    "short_name": "US",
+                    "types": ["country", "political"],
+                },
+                {"long_name": "98195", "short_name": "98195", "types": ["postal_code"]},
+            ],
+            "formatted_address": "Seattle, WA 98195, USA",
+            "geometry": {
+                "location": {"lat": 47.65533509999999, "lng": -122.3035199},
+                "location_type": "GEOMETRIC_CENTER",
+                "viewport": {
+                    "northeast": {"lat": 47.65668408029149, "lng": -122.3021709197085},
+                    "southwest": {"lat": 47.65398611970849, "lng": -122.3048688802915},
+                },
+            },
+            "place_id": "ChIJ6zWFnZIUkFQRoyu4AXksdGs",
+            "plus_code": {
+                "compound_code": "MM4W+4H Seattle, Washington, United States",
+                "global_code": "84VVMM4W+4H",
+            },
+            "types": ["establishment", "point_of_interest", "university"],
+        }
+    ]
+
+    def setUp(self):
+        super().setUp()
+        self.result = GeocodeResult(self.TEST_GEOCODE_JSON)
+
+    def test_latitude(self):
+        self.assertEqual(self.result.latitude, 47.65533509999999)
+
+    def test_longitude(self):
+        self.assertEqual(self.result.longitude, -122.3035199)
+
+    def test_formatted_address(self):
+        self.assertEqual(self.result.formatted_address, "Seattle, WA 98195, USA")
+
+    def test_street_address_1(self):
+        self.assertEqual(self.result.street_address_1, None)
+
+    def test_street_address_2(self):
+        self.assertEqual(self.result.street_address_2, None)
+
+    def test_city(self):
+        self.assertEqual(self.result.city, "Seattle")
+
+    def test_state(self):
+        self.assertEqual(self.result.state, "WA")
+
+    def test_country(self):
+        self.assertEqual(self.result.country, "US")
+
+    def test_postal_code(self):
+        self.assertEqual(self.result.postal_code, "98195")
+
+    def test_zip5(self):
+        self.assertEqual(self.result.zip5, "98195")
+
+    def test_is_complete(self):
+        self.assertFalse(self.result.is_complete)
+
