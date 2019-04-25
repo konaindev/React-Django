c782e4c698639d6044e330dc1704c74235fd0ccb
Dave Peck <davepeck@gmail.com>
2019-04-25 16:58:32 -0700

    Improve geo tests a tiny bit

--- remark/geo/tests.py
+++ remark/geo/tests.py
@@ -10,6 +10,12 @@ class AddressManagerTestCase(TestCase):
         result = GeocodeResult(TestCompleteGeocodeResult.TEST_GEOCODE_JSON)
         address = Address.objects.create_with_geocode_result(result)
         self.assertTrue(address is not None)
+        self.assertEqual(
+            address.geocode_result.geocode_json,
+            TestCompleteGeocodeResult.TEST_GEOCODE_JSON,
+        )
+        self.assertEqual(address.latitude, address.geocode_result.latitude)
+        self.assertEqual(address.longitude, address.geocode_result.longitude)
 
     def test_incomplete_result(self):
         result = GeocodeResult(TestIncompleteGeocodeResult.TEST_GEOCODE_JSON)
