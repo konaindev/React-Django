68f0b7117f776844e123d87d9b34a27185613a6c
Dave Peck <davepeck@gmail.com>
2019-04-26 11:52:04 -0700

    Implement simple geocoding for admin view

--- remark/geo/admin.py
+++ remark/geo/admin.py
@@ -1,12 +1,34 @@
 from django.contrib import admin
 
 from remark.admin import admin_site
+
+from .forms import LocationForm, AddressForm
 from .models import Address, Country, State, City
 
 
 @admin.register(Address, site=admin_site)
 class AddressAdmin(admin.ModelAdmin):
-    readonly_fields = ("geocode_json",)
+    ADDRESS_FIELDS = [
+        "street_address_1",
+        "street_address_2",
+        "city",
+        "state",
+        "zip_code",
+        "country",
+        "geocode_json",
+    ]
+
+    LOCATION_FIELD = ["location"]
+
+    def get_form(self, request, obj=None, **kwargs):
+        kwargs["form"] = LocationForm if obj is None else AddressForm
+        return super().get_form(request, obj, **kwargs)
+
+    def get_fields(self, request, obj=None):
+        return self.LOCATION_FIELD if obj is None else self.ADDRESS_FIELDS
+
+    def get_readonly_fields(self, request, obj=None):
+        return [] if obj is None else self.ADDRESS_FIELDS
 
 
 @admin.register(Country, site=admin_site)
