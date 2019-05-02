2004e198f53ac0ce43c7b22b3af47383665c81a4
mingthegit <ming.dev1026@gmail.com>
2019-05-02 22:41:58 +0800

    updated spreadsheet activator to populate zipcode outlines

--- remark/projects/spreadsheets/activators/market.py
+++ remark/projects/spreadsheets/activators/market.py
@@ -1,14 +1,12 @@
 from ..kinds import SpreadsheetKind
 from .json_activators import JSONFieldActivator
-
+from remark.geo.models import ZipcodePolygon
 
 class MarketActivator(JSONFieldActivator):
     spreadsheet_kind = SpreadsheetKind.MARKET
     project_field = "tmp_market_report_json"
 
     def activate_outlines(self):
-        # TODO ming:
-        #
         # At this point, self.data will contain imported data
         # from a TAM spreadsheet.
         #
@@ -16,13 +14,18 @@ class MarketActivator(JSONFieldActivator):
         # *THEN* you will want to set a value for the "outline" key
         # of each one:
         #
-        # population_zip_codes = self.data["estimated_population"].get("zip_codes", [])
-        # for population_zip_code in population_zip_codes:
-        #   zip = population_zip_code["zip"]
-        #   population_zip_code["outline"] = look_up_geo_outline(zip)
-        pass
+        population_zip_codes = self.data["estimated_population"].get("zip_codes", [])
+        for population_zip_code in population_zip_codes:
+            zip = population_zip_code["zip"]
+            zipcode_data = ZipcodePolygon.objects.fetch_zip_polygons(zip)
+
+            if zipcode_data is None:
+                population_zip_code["properties"] = None
+                population_zip_code["outline"] = None
+            else:
+                population_zip_code["properties"] = zipcode_data["properties"]
+                population_zip_code["outline"] = zipcode_data["geometry"]
 
     def activate(self):
         self.activate_outlines()
         super().activate()
-
