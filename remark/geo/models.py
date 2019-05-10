dbbcf7b3dd82273d2387eb9b1887a4b365998058
Leonardo Courbassier <leonardo.courbassier@gmail.com>
2019-05-10 13:09:45 -0300

    Add admin pages for USA census models

--- remark/geo/models.py
+++ remark/geo/models.py
@@ -196,6 +196,9 @@ class USACensusZip(models.Model):
 
     zipcode = models.CharField(max_length=20, unique=True)
 
+    def __str__(self):
+        return zipcode
+
 
 class USACensusPopulationByAge(models.Model):
     usa_census_zip = models.ForeignKey(
