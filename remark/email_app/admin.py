de01fd4565a97e3cf8523ca984508455cf513d64
Todd Cullen <culltp1@gmail.com>
2019-05-14 14:09:20 -0700

    adding error handling for sendgrid

--- remark/email_app/admin.py
+++ remark/email_app/admin.py
@@ -3,6 +3,7 @@ from .models import PerformanceEmail, PerformanceEmailKPI
 from remark.admin import admin_site
 from .reports.weekly_performance import send_performance_email
 import datetime
+from remark.lib.logging import error_text
 
 class PerformanceEmailKPIInline(admin.TabularInline):
     model = PerformanceEmailKPI
@@ -26,11 +27,17 @@ class PerformanceEmailAdmin(admin.ModelAdmin):
 
     def save_model(self, request, obj, form, change):
         print("email_app::admin::PerformanceEmailAdmin::save_model::top")
-        if not change:
-            obj.created_by = request.user
-        obj.end = obj.start + datetime.timedelta(days=7)
-        print("email_app::admin::PerformanceEmailAdmin::save_model::before save")
-        super().save_model(request, obj, form, change)
-        print("email_app::admin::PerformanceEmailAdmin::save_model::after save")
-        send_performance_email.apply_async(args=(obj.id,), countdown=2)
-        print("email_app::admin::PerformanceEmailAdmin::save_model::after async task")
+
+        try:
+            if not change:
+                obj.created_by = request.user
+            obj.end = obj.start + datetime.timedelta(days=7)
+            print("email_app::admin::PerformanceEmailAdmin::save_model::before save")
+            super().save_model(request, obj, form, change)
+            print("email_app::admin::PerformanceEmailAdmin::save_model::after save")
+            send_performance_email.apply_async(args=(obj.id,), countdown=2)
+            print("email_app::admin::PerformanceEmailAdmin::save_model::after async task")
+        except Exception as e:
+            print(error_text(e))
+
+        print("email_app::admin::PerformanceEmailAdmin::save_model::bottom")
