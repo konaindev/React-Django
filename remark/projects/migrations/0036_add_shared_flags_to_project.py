cbe9bd283e3106cf195d2db623d3fa9708127a14
mingthegit <ming.dev1026@gmail.com>
2019-05-21 09:57:10 +0800

    resolved migration conflict

--- remark/projects/migrations/0036_add_shared_flags_to_project.py
+++ remark/projects/migrations/0036_add_shared_flags_to_project.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('projects', '0034_merge_20190510_2127'),
+        ('projects', '0035_project_view_group'),
     ]
 
     operations = [
