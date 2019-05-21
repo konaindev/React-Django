62bec2c4803806a31d6bf2863cd00905bd0efea5
mingthegit <ming.dev1026@gmail.com>
2019-05-21 11:13:47 +0800

    updated zipcode importer to import lat/lon, land/water area

--- remark/geo/management/commands/import_zipcode_polygons.py
+++ remark/geo/management/commands/import_zipcode_polygons.py
@@ -114,23 +114,22 @@ def import_data_for_a_state(state):
     counter = 0
 
     for feature in data["features"]:
-        all_props = feature["properties"]
-        if all_props is None:
+        properties = feature["properties"]
+        if properties is None:
             continue
 
         counter = counter + 1
-        zip_code = all_props["ZCTA5CE10"]
-        lat = float(all_props["INTPTLAT10"])
-        lon = float(all_props["INTPTLON10"])
-        properties = dict(center=[lon, lat])
 
         ZipcodePolygon.objects.update_or_create(
-            zip_code=zip_code,
-            defaults={
-                "state": state.upper(),
-                "geometry": feature["geometry"],
-                "properties": properties,
-            },
+            zip_code=properties["ZCTA5CE10"],
+            defaults=dict(
+                state=state.upper(),
+                geometry=feature["geometry"],
+                lat=properties["INTPTLAT10"],
+                lon=properties["INTPTLON10"],
+                land_area=properties["ALAND10"],
+                water_area=properties["AWATER10"]
+            ),
         )
 
     return counter
