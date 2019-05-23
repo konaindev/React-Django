449e817395f8c9fc2ac56203270f1897b6f2ee27
mingthegit <ming.dev1026@gmail.com>
2019-05-23 17:13:21 +0800

    property row - added selection state

--- src/js/components/property_row/index.js
+++ src/js/components/property_row/index.js
@@ -1,5 +1,6 @@
-import PropTypes from "prop-types";
 import React from "react";
+import PropTypes from "prop-types";
+import cx from "classnames";
 
 import Panel from "../panel";
 import PropertyStatus from "../property_status";
@@ -10,11 +11,36 @@ const PropertyRow = ({
   image_url,
   property_name,
   address,
-  performance_rating
+  performance_rating,
+  selected,
+  selection_mode,
+  onSelect,
+  className,
+  style
 }) => {
+  const rowClass = cx(
+    className,
+    "property-row",
+    { "property-row--selection-mode": selection_mode },
+    { "property-row--selected": selected }
+  );
+
+  const imageStyle = {
+    backgroundImage: `url("${image_url}")`
+  };
+
+  const handleToggle = () => {
+    if (selection_mode === true) {
+      onSelect(!selected);
+    }
+  };
+
   return (
-    <Panel className="property-row">
-      <img className="property-row__image" src={image_url} />
+    <Panel className={rowClass} style={style}>
+      <div className="property-row__lead" onClick={handleToggle}>
+        <div style={imageStyle} className="property-row__image" />
+        <div className="property-row__tick" />
+      </div>
       <div className="property-row__name">{property_name}</div>
       <div className="property-row__address">{address}</div>
       <PropertyStatus performance_rating={performance_rating} />
@@ -26,7 +52,16 @@ PropertyRow.propTypes = {
   image_url: PropTypes.string.isRequired,
   property_name: PropTypes.string.isRequired,
   address: PropTypes.string.isRequired,
-  performance_rating: PropTypes.number.isRequired
+  performance_rating: PropTypes.number.isRequired,
+  selection_mode: PropTypes.bool.isRequired,
+  selected: PropTypes.bool.isRequired,
+  onSelect: PropTypes.func
+};
+
+PropertyRow.defaultProps = {
+  selection_mode: false,
+  selected: false,
+  onSelect: () => {}
 };
 
 export default React.memo(PropertyRow);
