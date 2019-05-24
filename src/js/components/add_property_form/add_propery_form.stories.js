ddfd02f4f2aced349e418b211275a2417601ca0b
Alexander Piterimov <piterimov@gmail.com>
2019-05-24 19:50:22 +0600

    Add skeleton for AddPropertyForm #165615919

---
+++ src/js/components/add_property_form/add_propery_form.stories.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+import { storiesOf } from "@storybook/react";
+
+import AddPropertyForm from "./index";
+
+storiesOf("AddPropertyForm", module).add("default", () => <AddPropertyForm />);
---
+++ src/js/components/add_property_form/index.js
@@ -0,0 +1,8 @@
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+
+export default class AddPropertyForm extends Component {
+  render() {
+    return <div>Form</div>;
+  }
+}
