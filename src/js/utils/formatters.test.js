c4f81c086a37ff51c0b671fae64152ed6b3ed9ae
Alexander Piterimov <piterimov@gmail.com>
2019-05-27 17:47:44 +0600

    Fix bug with dates formatting in Safary #166071190

--- src/js/utils/formatters.test.js
+++ src/js/utils/formatters.test.js
@@ -4,4 +4,8 @@ describe('utils > formatters', () => {
   it("formatDateWithTokens()", () => {
     expect(formatDateWithTokens("2018-12-17", "MMM D, YYYY")).toEqual("Dec 17, 2018");
   });
+
+  it("formatDateWithTokens with incorrect value", () => {
+    expect(formatDateWithTokens("Bad date", "MMM D, YYYY")).toEqual("Bad date");
+  });
 });
