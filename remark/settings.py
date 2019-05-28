5601d083d5946abce6aa9ad80622464f0445c010
Dave Peck <davepeck@gmail.com>
2019-05-28 10:19:20 -0700

    Remove ssl_require=True from settings.py

--- remark/settings.py
+++ remark/settings.py
@@ -175,7 +175,7 @@ LOGGING = {
 }
 
 # Change 'default' database configuration with $DATABASE_URL.
-DATABASES["default"].update(dj_database_url.config(conn_max_age=500, ssl_require=True))
+DATABASES["default"].update(dj_database_url.config(conn_max_age=500))
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
