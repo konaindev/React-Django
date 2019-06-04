40af3423edb2e0a75dedae8991b70a9f06ce21fd
Alexander Piterimov <piterimov@gmail.com>
2019-06-04 21:50:25 +0600

    Fix validation #165621043

--- src/js/components/add_property_form/validators.js
+++ src/js/components/add_property_form/validators.js
@@ -1,5 +1,5 @@
 import * as Yup from "yup";
 
-export const propertySchema = Yup.object.shape({
-  zipcode: Yup.number.integer()
+export const propertySchema = Yup.object().shape({
+  zipcode: Yup.number().integer()
 });
