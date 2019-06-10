ef77dd75e8ef7307abc7cabf6cc7f14709c32866
Alexey.Tsitrkop <alexey.tsitrkop@7bits.it>
2019-06-10 16:24:18 +0600

    Add close and search icons #165622696

--- src/js/icons/close.js
+++ src/js/icons/close.js
@@ -1,30 +1,21 @@
 import React from "react";
-
 import IconBase from "./icon_base";
 
-export function CloseSvgPath() {
-  return (
-    <>
-      <path
-        d="M1.43532 1.92201L11.3649 12.0613"
-        stroke="#5cf6dd"
-        stroke-width="2"
-        stroke-linecap="round"
-      />
-      <path
-        d="M11.4774 1.90854L1.83659 12.0504"
-        stroke="#5cf6dd"
-        stroke-width="2"
-        stroke-linecap="round"
-      />
-    </>
-  );
-}
+const Close = props => (
+  <IconBase viewBox="0 0 13 14" {...props}>
+    <path
+      d="M1.43532 1.92201L11.3649 12.0613"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+    />
+    <path
+      d="M11.4774 1.90854L1.83659 12.0504"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+    />
+  </IconBase>
+);
 
-export default function Close(props) {
-  return (
-    <IconBase viewBox="0 0 13 14" fill="none" {...props}>
-      <CloseSvgPath />
-    </IconBase>
-  );
-}
+export default Close;
--- src/js/icons/index.js
+++ src/js/icons/index.js
@@ -4,3 +4,5 @@ export { default as ArrowUp } from "./arrow_up";
 export { default as Close } from "./close";
 export { default as ListView } from "./list_view";
 export { default as TileView } from "./tile_view";
+export { default as Close } from "./close";
+export { default as Search } from "./search";
