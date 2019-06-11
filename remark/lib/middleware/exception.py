f123c5409d09546ecbbfb50e6f2828f06b0e8b82
Dave Peck <davepeck@gmail.com>
2019-06-11 15:00:33 -0700

    Bugfix the log_500 middleware; it would throw an exception of its own!

--- remark/lib/middleware/exception.py
+++ remark/lib/middleware/exception.py
@@ -1,6 +1,9 @@
+from django.http import HttpResponse
 from remark.lib.logging import getLogger, error_text
 
+
 logger = getLogger(__name__)
+
 
 def log_500(get_response):
     def middleware(request):
@@ -11,4 +14,5 @@ def log_500(get_response):
             logger.error(f"500 Error: {txt}")
             return HttpResponse(status=500)
         return response
+
     return middleware
