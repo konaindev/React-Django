e44d9b8d5a31b9702e42dbc1822a53d009b6d00d
Todd Cullen <culltp1@gmail.com>
2019-06-11 09:53:03 -0700

    adding logging to spreadsheet upload

--- remark/projects/forms.py
+++ remark/projects/forms.py
@@ -10,6 +10,10 @@ from .models import Project, Spreadsheet
 from .reports.selectors import ReportLinks
 from .spreadsheets import get_importer_for_kind, SpreadsheetKind
 
+from remark.lib.logging import error_text, getLogger
+
+logger = getLogger(__name__)
+
 
 class SpreadsheetForm(forms.ModelForm):
     """
@@ -21,28 +25,32 @@ class SpreadsheetForm(forms.ModelForm):
     def clean(self):
         cleaned_data = super().clean()
 
-        if cleaned_data["kind"] != SpreadsheetKind.MODELING and cleaned_data["subkind"]:
-            raise ValidationError(
-                "For non-modeling spreadsheets, 'subkind' must be blank."
-            )
+        try:
+            if cleaned_data["kind"] != SpreadsheetKind.MODELING and cleaned_data["subkind"]:
+                raise ValidationError(
+                    "For non-modeling spreadsheets, 'subkind' must be blank."
+                )
 
-        # Attempt to import and validate the spreadsheet contents
-        importer = get_importer_for_kind(cleaned_data["kind"], cleaned_data["file"])
-        if importer is None:
-            raise ValidationError(
-                f"No spreadsheet importer available for {cleaned_data['kind']}"
-            )
-        if not importer.is_valid():
-            raise ValidationError(f"Could not validate spreadsheet: {importer.errors}")
+            # Attempt to import and validate the spreadsheet contents
+            importer = get_importer_for_kind(cleaned_data["kind"], cleaned_data["file"])
+            if importer is None:
+                raise ValidationError(
+                    f"No spreadsheet importer available for {cleaned_data['kind']}"
+                )
+            if not importer.is_valid():
+                raise ValidationError(f"Could not validate spreadsheet: {importer.errors}")
 
-        # If this is a modeling spreadsheet, set the subkind based on the model name.
-        if cleaned_data["kind"] == SpreadsheetKind.MODELING:
-            cleaned_data["subkind"] = importer.cleaned_data["name"]
+            # If this is a modeling spreadsheet, set the subkind based on the model name.
+            if cleaned_data["kind"] == SpreadsheetKind.MODELING:
+                cleaned_data["subkind"] = importer.cleaned_data["name"]
 
-        # Success! We read the spreadsheet just fine.
-        cleaned_data["imported_data"] = importer.cleaned_data
+            # Success! We read the spreadsheet just fine.
+            cleaned_data["imported_data"] = importer.cleaned_data
 
-        return cleaned_data
+            return cleaned_data
+        except Exception as e:
+            logger.error(error_as_text(e))
+            raise Exception("Invalid spreadsheet")
 
     class Meta:
         model = Spreadsheet
