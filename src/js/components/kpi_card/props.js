392df583ae9cec0e5b0fdd0d4d77baafb7176eb1
Alexey.Tsitrkop <alexey.tsitrkop@7bits.it>
2019-06-21 12:11:46 +0600

    replac -> to &rarr; #166558318

--- src/js/components/kpi_card/props.js
+++ src/js/components/kpi_card/props.js
@@ -1,20 +1,20 @@
 export const offTrack = {
   health: 0,
-  name: "TOU -> APP",
+  name: "TOU → APP",
   target: "66%",
   value: "33%"
 };
 
 export const atRisk = {
   health: 1,
-  name: "INQ -> TOU",
+  name: "INQ → TOU",
   target: "40%",
   value: "23%"
 };
 
 export const onTrack = {
   health: 2,
-  name: "USV -> INQ",
+  name: "USV → INQ",
   target: "6%",
   value: "5%"
 };
