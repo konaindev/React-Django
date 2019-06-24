93f0e95732cf23d5719424c910778a3345664dbc
Alexander Piterimov <piterimov@gmail.com>
2019-06-24 20:37:04 +0600

    Implementing svg chart #166420109

--- remark/charts/views.py
+++ remark/charts/views.py
@@ -1,5 +1,7 @@
-from django.http import JsonResponse
-from django.shortcuts import render
+from cairosvg import svg2png
+from django.http import HttpResponse, JsonResponse
+from django.template.loader import render_to_string
+from svgwrite.path import Path
 
 from remark.lib.views import RemarkView
 
@@ -9,7 +11,13 @@ from .forms import DonutForm
 class DonutPieChartView(RemarkView):
     def get(self, request):
         form = DonutForm(request.GET)
-        # if not form.is_valid():
-        #     return JsonResponse(form.errors.get_json_data(), status=400)
-        # data = form.cleaned_data
-        return render(request, "donut.svg", {}, content_type="image/svg+xml")
+        if not form.is_valid():
+            return JsonResponse(form.errors.get_json_data(), status=400)
+        data = form.cleaned_data
+        start = "M249.5 34.0497"
+        current_path = '<path d="M249.5 34.0497 A215.4503 215.4503 0 1 1 109 249.5" fill="red" />'
+        data['current_path'] = current_path
+        svg = render_to_string("donut.svg", data)
+        # import pdb; pdb.set_trace()
+        # png = svg2png(svg.encode("utf-8"))
+        return HttpResponse(svg, content_type="image/svg+xml")
