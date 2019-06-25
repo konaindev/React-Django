2e1116275981ef6199a2b04bfee9bb329e6aa86a
Alexey.Tsitrkop <alexey.tsitrkop@7bits.it>
2019-06-25 16:01:36 +0600

    Add reset button to GroupSelect #166887166

--- src/js/components/group_select/index.js
+++ src/js/components/group_select/index.js
@@ -5,6 +5,7 @@ import PropTypes from "prop-types";
 import React from "react";
 import { components } from "react-select";
 
+import Button from "../button";
 import Checkbox from "../checkbox";
 import Select from "../select";
 
@@ -45,27 +46,49 @@ function Control(props) {
 
 export default class GroupSelect extends React.PureComponent {
   menuList = props => {
+    const classes = cn(
+      "group-select__all",
+      "group-select__option",
+      "select__option",
+      {
+        "select__option--is-selected": this.isAllSelected
+      }
+    );
     return (
       <components.MenuList {...props}>
-        <div
-          className="group-select__all group-select__option select__option"
-          onClick={this.onSelectAll}
-        >
+        <div className={classes} onClick={this.onSelectAll}>
           <Checkbox isSelected={this.isAllSelected} />
           <div className="group-select__option-label">
             {this.props.selectAllLabel}
           </div>
         </div>
         <div className="group-select__groups">{props.children}</div>
+        <div className="group-select__controls">
+          <Button
+            className="group-select__reset"
+            uppercase={true}
+            onClick={this.onReset}
+          >
+            reset
+          </Button>
+        </div>
       </components.MenuList>
     );
   };
 
   groupHeading = props => {
+    const classes = cn(
+      "group-select__group",
+      "group-select__option",
+      "select__option",
+      {
+        "select__option--is-selected": this.isGroupSelect(props.children)
+      }
+    );
     return (
       <components.GroupHeading {...props}>
         <div
-          className="group-select__group group-select__option select__option"
+          className={classes}
           onClick={() => this.onSelectGroup(props.children)}
         >
           <Checkbox isSelected={this.isGroupSelect(props.children)} />
@@ -86,6 +109,10 @@ export default class GroupSelect extends React.PureComponent {
     const groupOptions = this.props.options.find(o => o.label === group)
       .options;
     return !_differenceBy(groupOptions, this.props.value, "value").length;
+  };
+
+  onReset = () => {
+    this.props.onChange([]);
   };
 
   onSelectAll = () => {
