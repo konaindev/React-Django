fb6da968ef99d843a487f33a6eacd985d45c4227
mingthegit <ming.dev1026@gmail.com>
2019-06-26 10:24:08 +0800

    test for ZipcodeManger.look_up_polygon

--- remark/geo/tests.py
+++ remark/geo/tests.py
@@ -1,7 +1,9 @@
+from decimal import *
+
 from django.test import TestCase
 
 from .geocode import GeocodeResult
-from .models import Address
+from .models import Address, Zipcode
 from .test_geocode import TestCompleteGeocodeResult, TestIncompleteGeocodeResult
 
 
@@ -26,3 +28,50 @@ class AddressManagerTestCase(TestCase):
         result = None
         address = Address.objects.create_with_geocode_result(result)
         self.assertTrue(address is None)
+
+class ZipcodeManagerTestCase(TestCase):
+    """
+    Cover geo.Zipcode model
+    """
+
+    def setUp(self):
+        zipcode = Zipcode.objects.update_or_create(
+            zip_code="97201",
+            defaults=dict(
+                state="OR",
+                geometry={
+                    "type": "Polygon",
+                    "coordinates": [
+                        [
+                            [-122.713058, 45.476042],
+                            [-122.720757, 45.520582],
+                            [-122.700411, 45.523881],
+                            [-122.680616, 45.515083],
+                            [-122.672918, 45.513983],
+                            [-122.667419, 45.504635],
+                            [-122.669619, 45.476042],
+                            [-122.713058, 45.476042]
+                        ]
+                    ]
+                },
+                lat=45.482741,
+                lon=-122.644441,
+                land_area=6.326849507380239,
+                water_area=1.0234371306268757
+            )
+        )
+
+    def test_look_up_polygon(self):
+        jsonable = Zipcode.objects.look_up_polygon("97201")
+        self.assertTrue(jsonable is not None)
+        self.assertEqual(type(jsonable), dict)
+        self.assertEqual(type(jsonable["geometry"]), dict)
+        self.assertEqual(type(jsonable["properties"]), dict)
+
+        center = jsonable["properties"]["center"]
+        self.assertEqual(type(center), list)
+        self.assertEqual(type(center[0]), Decimal)
+        self.assertEqual(type(center[1]), Decimal)
+
+        jsonable = Zipcode.objects.look_up_polygon("missing_zip")
+        self.assertTrue(jsonable is None)
