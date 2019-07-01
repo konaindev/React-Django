c9f836afe6e3ff98a382093c9e8e063449e926f2
Alexey.Tsitrkop <alexey.tsitrkop@7bits.it>
2019-07-01 17:27:30 +0600

    Add PortfolioAnalysisView Component #166586987

---
+++ src/js/components/portfolio_analysis_view/portfolio_analysis_view.stories.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { withState } from "@dump247/storybook-state";
+import { action } from "@storybook/addon-actions";
+import { storiesOf } from "@storybook/react";
+
+import PortfolioAnalysisView from "./index";
+import { props } from "./props";
+
+const initState = {
+  selected_kpi_bundle: props.selected_kpi_bundle,
+  date_selection: props.date_selection
+};
+
+storiesOf("PortfolioAnalysisView", module).add(
+  "default",
+  withState(initState)(({ store }) => (
+    <PortfolioAnalysisView
+      {...props}
+      {...store.state}
+      onChange={props => {
+        store.set({ ...props });
+        action("onChange")(props);
+      }}
+    />
+  ))
+);
