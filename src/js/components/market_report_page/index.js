0ef65731c1a506a89ee215e5f97a3d7f5eba939e
Jason Child <jason@Jasons-MBP.hsd1.wa.comcast.net>
2019-07-03 08:28:33 -0700

    fix(MarketReportPage): connect to redux store (missed on initial effort)

--- src/js/components/market_report_page/index.js
+++ src/js/components/market_report_page/index.js
@@ -3,8 +3,9 @@ import PropTypes from "prop-types";
 
 import ReportPageChrome from "../report_page_chrome";
 import TotalAddressableMarket from "../total_addressable_market";
+import { connect } from "react-redux";
 
-export default class MarketReportPage extends Component {
+export class MarketReportPage extends Component {
   static propTypes = {
     report: PropTypes.object.isRequired,
     project: PropTypes.object.isRequired
@@ -25,3 +26,5 @@ export default class MarketReportPage extends Component {
     );
   }
 }
+
+export default connect(x => x)(MarketReportPage);
