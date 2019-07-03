d6400147b0fd52dac25040a6bba4f7a25a4c3a28
Jason Child <jason@Jasons-MBP.hsd1.wa.comcast.net>
2019-07-03 08:17:27 -0700

    chore(redux): enable redux devtools tracing

--- src/js/state/store/index.js
+++ src/js/state/store/index.js
@@ -1,7 +1,9 @@
 import { createStore } from "redux";
 import reducers from "../reducers";
 
+// TODO: contextually enable devtools based on prod or not
 export default createStore(
   reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__({ trace: true, traceLimit: 25 })
 );
