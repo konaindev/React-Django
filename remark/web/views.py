91cb29e4dc9a1796e47da7d9651bee43df95fe61
Jason Child <jason@jasons-mbp.lan>
2019-07-08 11:07:46 -0700

    feat(cache): add per-route-handler caching to /dashboard

--- remark/web/views.py
+++ remark/web/views.py
@@ -5,6 +5,8 @@ from remark.geo.models import State
 from remark.projects.models import Fund, Project
 from remark.lib.views import ReactView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.cache import cache
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
 
 
 def has_property_in_list_of_dict(ary, prop, value):
@@ -12,6 +14,7 @@ def has_property_in_list_of_dict(ary, prop, value):
         if item[prop] == value:
             return True
     return False
+
 
 class DashboardView(LoginRequiredMixin, ReactView):
     """Render dashboard page."""
@@ -33,6 +36,14 @@ class DashboardView(LoginRequiredMixin, ReactView):
     def get(self, request):
         user = request.user
 
+        cache_key = "{}^{}^{}".format(
+            user.public_id, request.path, request.META["QUERY_STRING"]
+        )
+
+        if cache_key in cache:
+            cached_reponse = cache.get(cache_key)
+            return cached_reponse
+
         if user.is_superuser:
             project_params = {}
         else:
@@ -48,35 +59,38 @@ class DashboardView(LoginRequiredMixin, ReactView):
             if project.address is not None:
                 state = project.address.state
                 city = project.address.city
-                label = f"{city}, {state.upper()}",
+                label = (f"{city}, {state.upper()}",)
                 if not has_property_in_list_of_dict(locations, "label", label):
                     locations.append(
-                        {
-                            "city": city,
-                            "label": label,
-                            "state": state.lower(),
-                        }
+                        {"city": city, "label": label, "state": state.lower()}
                     )
-            if project.asset_manager is not None and \
-                not has_property_in_list_of_dict(asset_managers, "id", project.asset_manager.public_id):
-                asset_managers.append({
-                    "id": project.asset_manager.public_id,
-                    "label": project.asset_manager.name
-                })
+            if project.asset_manager is not None and not has_property_in_list_of_dict(
+                asset_managers, "id", project.asset_manager.public_id
+            ):
+                asset_managers.append(
+                    {
+                        "id": project.asset_manager.public_id,
+                        "label": project.asset_manager.name,
+                    }
+                )
 
-            if project.property_manager is not None and \
-                not has_property_in_list_of_dict(property_managers, "id", project.property_manager.public_id):
-                property_managers.append({
-                    "id": project.property_manager.public_id,
-                    "label": project.property_manager.name
-                })
+            if (
+                project.property_manager is not None
+                and not has_property_in_list_of_dict(
+                    property_managers, "id", project.property_manager.public_id
+                )
+            ):
+                property_managers.append(
+                    {
+                        "id": project.property_manager.public_id,
+                        "label": project.property_manager.name,
+                    }
+                )
 
-            if project.fund is not None and \
-                not has_property_in_list_of_dict(funds, "id", project.fund.public_id):
-                funds.append({
-                    "id": project.fund.public_id,
-                    "label": project.fund.name
-                })
+            if project.fund is not None and not has_property_in_list_of_dict(
+                funds, "id", project.fund.public_id
+            ):
+                funds.append({"id": project.fund.public_id, "label": project.fund.name})
 
         if request.GET.get("q"):
             project_params["name__icontains"] = request.GET.get("q")
@@ -113,7 +127,9 @@ class DashboardView(LoginRequiredMixin, ReactView):
                 {
                     "property_name": project.name,
                     "property_id": project.public_id,
-                    "address": f"{project.address.city}, {project.address.state}" if project.address else "",
+                    "address": f"{project.address.city}, {project.address.state}"
+                    if project.address
+                    else "",
                     "image_url": project.get_building_image_url(),
                     "performance_rating": project.get_performance_rating(),
                     "url": project.get_baseline_url(),
@@ -122,9 +138,11 @@ class DashboardView(LoginRequiredMixin, ReactView):
 
         if sort == "performance":
             is_reverse = direction == "asc"
-            projects = sorted(projects, key=lambda p: p["performance_rating"], reverse=is_reverse)
+            projects = sorted(
+                projects, key=lambda p: p["performance_rating"], reverse=is_reverse
+            )
 
-        return self.render(
+        response = self.render(
             no_projects=no_projects,
             properties=projects,
             user=user_dict,
@@ -134,3 +152,5 @@ class DashboardView(LoginRequiredMixin, ReactView):
             asset_managers=asset_managers,
             funds=funds,
         )
+        cache.set(cache_key, response, timeout=DEFAULT_TIMEOUT)
+        return response
