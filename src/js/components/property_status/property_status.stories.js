e33228f74e0ca4db8206b8385d70895cc157e307
konaindev <konainshah00009@gmail.com>
2019-07-15 17:55:06 -0700

    Revert "Fix migrations 1"

--- src/js/components/property_status/property_status.stories.js
+++ src/js/components/property_status/property_status.stories.js
@@ -4,15 +4,13 @@ import { storiesOf } from "@storybook/react";
 import PropertyStatus from "./index";
 
 storiesOf("PropertyStatus", module)
-  .add("OFF TRACK", () => <PropertyStatus performance_rating={0} />)
+  .add("NEEDS REVIEW", () => <PropertyStatus performance_rating={0} />)
   .add("AT RISK", () => <PropertyStatus performance_rating={1} />)
   .add("ON TRACK", () => <PropertyStatus performance_rating={2} />)
-  .add("CAMPAIGN PENDING", () => <PropertyStatus performance_rating={-1} />)
   .add("All", () => (
     <React.Fragment>
       <PropertyStatus performance_rating={0} />
       <PropertyStatus performance_rating={1} />
       <PropertyStatus performance_rating={2} />
-      <PropertyStatus performance_rating={-1} />
     </React.Fragment>
   ));
