0def1715adcdb2478a1d1973d5760d7de3180f9a
Alexey.Tsitrkop <alexey.tsitrkop@7bits.it>
2019-07-17 20:07:03 +0600

    Add InvestmentAllocation component #164790046

---
+++ src/js/components/investment_allocation/props.js
@@ -0,0 +1,41 @@
+export const props = {
+  acquisition: {
+    expenses: {
+      reputation_building: "20400.00",
+      demand_creation: "48000.00",
+      leasing_enablement: "0.00",
+      market_intelligence: "0.00"
+    },
+    total: "68400.00"
+  },
+  retention: {
+    expenses: {
+      reputation_building: "0.00",
+      demand_creation: "0.00",
+      leasing_enablement: "0.00",
+      market_intelligence: "0.00"
+    },
+    total: "0.00"
+  }
+};
+
+export const propsFill = {
+  acquisition: {
+    expenses: {
+      reputation_building: "20400.00",
+      demand_creation: "48000.00",
+      leasing_enablement: "0.00",
+      market_intelligence: "0.00"
+    },
+    total: "68400.00"
+  },
+  retention: {
+    expenses: {
+      reputation_building: "0.00",
+      demand_creation: "0.00",
+      leasing_enablement: "6000.00",
+      market_intelligence: "0.00"
+    },
+    total: "6000.00"
+  }
+};
