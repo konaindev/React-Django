ddf5b69f6e58bd65a32a225bfa53c867631690e2
Todd Cullen <culltp1@gmail.com>
2019-07-19 12:16:13 -0700

    temp commit

--- remark/lib/time_series/granularity.py
+++ remark/lib/time_series/granularity.py
@@ -1,7 +1,8 @@
+from decimal import Decimal
 
 from .query import select
 
-def merge(merge_document, base_query, start, end):
+def merge(merge_document, base_query, start, end, hydrater=None):
     query = select(base_query, start, end)
     ts = list(query)
     length = len(ts)
@@ -14,17 +15,55 @@ def merge(merge_document, base_query, start, end):
     else:
         pass
 
-def trim(merge_document, p, num_seconds):
-    result = {}
+
+LEFT = 0
+RIGHT = 1
+
+def split(merge_document, p, when):
+    result_left = {}
+    result_right = {}
     for property in merge_document.keys():
         method = merge_document[property]
         if method == "linear":
+            result_left[property], result_right[property] = split_linear(p, property, when)
+        elif callable(method):
+            result_left[property], result_right[property] = method(p, property, when)
+        else:
+            raise Exception(f"Split method not found: {method}")
+    return result_left, result_right
 
-        result[property] = merge_document
 
-
-def trim_linear(p, property, num_seconds):
-    value = getattr(p, property)
+def split_linear(p, property, when):
     total_seconds = (p.end - p.start).total_seconds()
-    percent = num_seconds / total_seconds
-    return value * percent
+    left_seconds = (when - p.start).total_seconds()
+
+    value = getattr(p, property)
+    kind = time_value_type(value)
+    if kind == int:
+        left_value = round(value * (left_seconds / total_seconds))
+    elif kind == float:
+        left_value = value * (left_seconds / total_seconds)
+    elif kind == Decimal:
+        left_value = value * (
+            Decimal(left_seconds) / Decimal(total_seconds)
+        )
+    else:
+        left_value = None
+
+    right_value = value - left_value if left_value is not None else None
+
+    return left_value, right_value
+
+def time_value_type(value=None):
+    """
+    Take a guess at the type of the underlying value for a set of
+    time_values, if possible.
+
+    Returns the type of the first value that isn't None; return None
+    if no type can be determined.
+    """
+    return (
+        None
+        if (value is None)
+        else type(value)
+    )
--- remark/lib/time_series/query.py
+++ remark/lib/time_series/query.py
@@ -2,14 +2,27 @@ from django.db.models import Q
 
 
 # Do I want to provide an expansion document
+# Mostly likely yes. To Hydrate the model for point values.
+# Also, to pull the model data out of the model class into a raw data structure
+
 
 '''
 This function produces a query that will select all periods
 of data that touch the time period specified by start and end.
 '''
-def select(base_query, start, end):
+def select(base_query, start, end, hydrater=None):
     before = (Q(start__gte=start) & Q(start__lt=end))
     middle = (Q(start__lte=start) & Q(end__gt=start))
     after = (Q(end__gt=start) & Q(end__lt=end))
 
-    return base_query.filter(Q(before | middle | after)).order_by("end")
+    query = base_query.filter(Q(before | middle | after)).order_by("end")
+    ts = list(query)
+    if hydrater is not None:
+        result = []
+        for item in ts:
+            result.append(hydrater(item))
+    else:
+        return ts
+    return result
+
+
