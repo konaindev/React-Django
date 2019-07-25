02623315a4ea1287e440f3d8492174139243f64c
Alexey.Tsitrkop <alexey.tsitrkop@7bits.it>
2019-07-25 11:03:38 +0600

    Fix migration #166888394

--- remark/projects/migrations/0058_add_include_in_remarkably_averages_to_project.py
+++ remark/projects/migrations/0058_add_include_in_remarkably_averages_to_project.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [("projects", "0056_remove_users_from_project")]
+    dependencies = [("projects", "0057_merge_20190719_0759")]
 
     operations = [
         migrations.AddField(
