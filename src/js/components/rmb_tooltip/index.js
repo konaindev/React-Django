e6e25e7c2b8593e1adee4546f6d39fde9c4ade41
Alexey.Tsitrkop <alexey.tsitrkop@7bits.it>
2019-07-30 19:21:05 +0600

    Add new theme 'light' for RMBTooltip #167154878

--- src/js/components/rmb_tooltip/index.js
+++ src/js/components/rmb_tooltip/index.js
@@ -1,11 +1,20 @@
+import cn from "classnames";
+import PropTypes from "prop-types";
 import React from "react";
 import Tooltip from "rc-tooltip";
 
 import "rc-tooltip/assets/bootstrap.css";
 import "./rmb_tooltip.scss";
 
-export const RMBTooltip = props => {
-  return <Tooltip {...props} overlayClassName="rmb-tooltip" />;
+export const RMBTooltip = ({ theme, ...props }) => {
+  const classes = cn("rmb-tooltip", {
+    [`rmb-tooltip--${theme}`]: theme
+  });
+  return <Tooltip {...props} overlayClassName={classes} />;
+};
+
+RMBTooltip.propTypes = {
+  theme: PropTypes.oneOf(["", "light"])
 };
 
 export default RMBTooltip;
--- src/js/components/rmb_tooltip/rmb_tooltip.stories.js
+++ src/js/components/rmb_tooltip/rmb_tooltip.stories.js
@@ -26,69 +26,69 @@ const containerStyle = {
   padding: "2rem"
 };
 
-const rcTooltipExample = (
+const rcTooltipExample = theme => (
   <div style={containerStyle}>
     <div style={rowStyle}>
-      <Tooltip placement="left" overlay={text}>
+      <Tooltip theme={theme} placement="left" overlay={text}>
         <a href="#" style={styles}>
           Left
         </a>
       </Tooltip>
-      <Tooltip placement="top" overlay={text}>
+      <Tooltip theme={theme} placement="top" overlay={text}>
         <a href="#" style={styles}>
           Top
         </a>
       </Tooltip>
-      <Tooltip placement="bottom" overlay={text}>
+      <Tooltip theme={theme} placement="bottom" overlay={text}>
         <a href="#" style={styles}>
           Bottom
         </a>
       </Tooltip>
-      <Tooltip placement="right" overlay={text}>
+      <Tooltip theme={theme} placement="right" overlay={text}>
         <a href="#" style={styles}>
           Right
         </a>
       </Tooltip>
     </div>
     <div style={rowStyle}>
-      <Tooltip placement="leftTop" overlay={text}>
+      <Tooltip theme={theme} placement="leftTop" overlay={text}>
         <a href="#" style={styles}>
           Left Top
         </a>
       </Tooltip>
-      <Tooltip placement="leftBottom" overlay={text}>
+      <Tooltip theme={theme} placement="leftBottom" overlay={text}>
         <a href="#" style={styles}>
           Left Bottom
         </a>
       </Tooltip>
-      <Tooltip placement="rightTop" overlay={text}>
+      <Tooltip theme={theme} placement="rightTop" overlay={text}>
         <a href="#" style={styles}>
           Right Top
         </a>
       </Tooltip>
-      <Tooltip placement="rightBottom" overlay={text}>
+      <Tooltip theme={theme} placement="rightBottom" overlay={text}>
         <a href="#" style={styles}>
           Right Bottom
         </a>
       </Tooltip>
     </div>
     <div style={rowStyle}>
-      <Tooltip placement="topLeft" overlay={text}>
+      <Tooltip theme={theme} placement="topLeft" overlay={text}>
         <a href="#" style={styles}>
           Top Left
         </a>
       </Tooltip>
-      <Tooltip placement="topRight" overlay={text}>
+      <Tooltip theme={theme} placement="topRight" overlay={text}>
         <a href="#" style={styles}>
           Top Right
         </a>
       </Tooltip>
-      <Tooltip placement="bottomLeft" overlay={text}>
+      <Tooltip theme={theme} placement="bottomLeft" overlay={text}>
         <a href="#" style={styles}>
           Bottom Left
         </a>
       </Tooltip>
-      <Tooltip placement="bottomRight" overlay={text}>
+      <Tooltip theme={theme} placement="bottomRight" overlay={text}>
         <a href="#" style={styles}>
           Bottom Right
         </a>
@@ -97,8 +97,14 @@ const rcTooltipExample = (
   </div>
 );
 
-storiesOf("RMBTooltip", module).add("base", () => (
-  <div style={{ margin: "1rem auto" }}>
-    <Container>{rcTooltipExample}</Container>
-  </div>
-));
+storiesOf("RMBTooltip", module)
+  .add("base", () => (
+    <div style={{ margin: "1rem auto" }}>
+      <Container>{rcTooltipExample()}</Container>
+    </div>
+  ))
+  .add("light", () => (
+    <div style={{ margin: "1rem auto" }}>
+      <Container>{rcTooltipExample("light")}</Container>
+    </div>
+  ));
