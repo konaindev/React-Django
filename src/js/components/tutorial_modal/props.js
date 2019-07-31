8adc0086bb33aa4100423b6306083793d3bffc3b
Alexey.Tsitrkop <alexey.tsitrkop@7bits.it>
2019-07-31 21:30:12 +0600

    Update image for storybook #167158684

--- src/js/components/tutorial_modal/props.js
+++ src/js/components/tutorial_modal/props.js
@@ -1,18 +1,22 @@
+import img_apartments from "../../../images/tutorial_images/apartments.png";
+import img_portfolio_analysis from "../../../images/tutorial_images/portfolio_analysis.png";
+import img_invite_users from "../../../images/tutorial_images/invite_users.png";
+
 export const props = {
   tutorials: [
     {
-      image_url: "https://imgur.com/vW7xfzx.png",
+      image_url: img_apartments,
       caption:
         "Keep tabs on all your properties and how they are performing towards their goal."
     },
     {
-      image_url: "https://imgur.com/MLfs4eQ.png",
+      image_url: img_portfolio_analysis,
       caption: "Have a bird's eye view of across your entire portfolio."
     },
     {
-      image_url: "https://imgur.com/eQtUTc0.png",
+      image_url: img_invite_users,
       caption:
-        "Invite members of your organization to stay up to date and colloborate across projects."
+        "Invite members of your organization to stay up to date and collaborate across projects."
     }
   ],
   title: "Quickstart"
