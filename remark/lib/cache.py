0b7afc52314134fbacadf0bc58a6b1683c4cacc6
Todd Cullen <culltp1@gmail.com>
2019-08-13 10:51:50 -0700

    adding some documentation

--- remark/lib/cache.py
+++ remark/lib/cache.py
@@ -7,6 +7,12 @@ TIMEOUT_1_WEEK = TIMEOUT_1_DAY * 7
 
 
 def remark_cache(base_key, timeout=TIMEOUT_1_HOUR, version=0):
+    """
+    :param base_key: the static part of the key - the dynamic arguments will get added to it
+    :param timeout: the cache timeout in seconds, there's a few helper constants in the library - not required defaults to one hour
+    :param version: the version of the cache in case you change the object being cached - not required, defaults to zero
+    :return: a decorator that will cache the result of the function when the params are the same
+    """
     def decorator(function):
         def wrapper(*args, **kwargs):
             key = f"{base_key}|"
