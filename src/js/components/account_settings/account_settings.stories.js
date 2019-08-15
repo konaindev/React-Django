9e7d7ddbd1ae9687e4307b3557634ba7100f670a
Alexander Piterimov <piterimov@gmail.com>
2019-08-15 17:55:52 +0600

    Add stories for account settings #167702891

--- src/js/components/account_settings/account_settings.stories.js
+++ src/js/components/account_settings/account_settings.stories.js
@@ -3,4 +3,7 @@ import { storiesOf } from "@storybook/react";
 
 import AccountSettings from "./index";
 
-storiesOf("AccountSettings", module).add("default", () => <AccountSettings />);
+storiesOf("AccountSettings", module)
+  .add("Profile", () => <AccountSettings initialTab="profile" />)
+  .add("Account Security", () => <AccountSettings initialTab="lock" />)
+  .add("Email Reports", () => <AccountSettings initialTab="email" />);
