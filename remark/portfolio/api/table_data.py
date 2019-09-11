55ae6276076e99999dae19977f3ce7a0f845339e
Alexander Piterimov <piterimov@gmail.com>
2019-09-12 00:39:30 +0600

    Return all projects even if there are no data #167905861

--- remark/portfolio/api/table_data.py
+++ remark/portfolio/api/table_data.py
@@ -5,6 +5,7 @@ from remark.lib.time_series.computed import generate_computed_kpis, generate_com
 from remark.lib.time_series.common import KPI, KPIFormat
 from .strategy import get_base_kpis_for_project, get_targets_for_project, get_base_kpis_for_group, get_targets_for_group
 
+
 def generate_computed_properties(item, kpis):
     base_kpis = item["base_kpis"]
     item["kpis"] = generate_computed_kpis(base_kpis, outputs=kpis)
@@ -57,36 +58,35 @@ def get_table_structure(user, start, end, kpis, show_averages):
         base_kpis = get_base_kpis_for_project(project, start, end)
         base_targets = get_targets_for_project(project, start, end)
 
-        if base_kpis is not None:
-            image_url = project.get_building_image()[2]
-            if image_url == "":
-                image_url = "https://s3.amazonaws.com/production-storage.remarkably.io/portfolio/all_my_properties.png"
+        image_url = project.get_building_image()[2]
+        if image_url == "":
+            image_url = "https://s3.amazonaws.com/production-storage.remarkably.io/portfolio/all_my_properties.png"
 
-            item = {
-                "id": project.public_id,
-                "type": "individual",
-                "name": project.name,
-                "address": f"{project.property.geo_address.city}, {project.property.geo_address.state}",
-                "image_url": image_url,
-                "url": reverse(
-                    "performance_report",
-                    kwargs={"project_id": project.public_id, "report_span": "last-four-weeks"}
-                ),
-                "health": project.get_performance_rating(),
-                "base_kpis": base_kpis,
-                "base_targets": base_targets
-            }
-            generate_computed_properties(item, kpis)
-            format_kpis(item, kpis)
-            project_flat_list.append(item)
+        item = {
+            "id": project.public_id,
+            "type": "individual",
+            "name": project.name,
+            "address": f"{project.property.geo_address.city}, {project.property.geo_address.state}",
+            "image_url": image_url,
+            "url": reverse(
+                "performance_report",
+                kwargs={"project_id": project.public_id, "report_span": "last-four-weeks"}
+            ),
+            "health": project.get_performance_rating(),
+            "base_kpis": base_kpis,
+            "base_targets": base_targets
+        }
+        generate_computed_properties(item, kpis)
+        format_kpis(item, kpis)
+        project_flat_list.append(item)
 
-            # generate table structure
-            custom_tags = list(project.custom_tags.all())
-            if len(custom_tags) > 0:
-                for custom_tag in custom_tags:
-                    if custom_tag.word not in groupings:
-                        groupings[custom_tag.word] = []
-                    groupings[custom_tag.word].append(project.public_id)
+        # generate table structure
+        custom_tags = list(project.custom_tags.all())
+        if len(custom_tags) > 0:
+            for custom_tag in custom_tags:
+                if custom_tag.word not in groupings:
+                    groupings[custom_tag.word] = []
+                groupings[custom_tag.word].append(project.public_id)
 
     print(f"Project Length: {len(project_flat_list)}")
     print(f"Project data: {project_flat_list}")
