9d321aa70ffcdb684fda98c35c53b4346a74f97a
Jason Child <jason@jasons-mbp.lan>
2019-09-12 13:08:40 -0700

    fix(linting): fix a linting error in storybook props

--- src/js/components/project_page/props.js
+++ src/js/components/project_page/props.js
@@ -4,7 +4,7 @@ export const project = {
   building_image: [
     "https://i.imgur.com/UEH4gfU.jpg",
     "https://i.imgur.com/UEH4gfU.jpg",
-    "https://i.imgur.com/UEH4gfU.jpg",
+    "https://i.imgur.com/UEH4gfU.jpg"
   ],
   health: 2,
   update_endpoint: "/projects/pro_example/update/"
