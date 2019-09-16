013a74540a1a9c3af53689c214303cdc51cc35a3
Alexander Piterimov <piterimov@gmail.com>
2019-09-16 21:32:06 +0600

    Add no data alarm for property row #167905861

--- src/js/components/portfolio_property_row/index.js
+++ src/js/components/portfolio_property_row/index.js
@@ -1,12 +1,14 @@
 import cn from "classnames";
+import _isNil from "lodash/isNil";
 import PropTypes from "prop-types";
 import React from "react";
 
-import PropertyStatus from "../property_status";
-
 import "./portfolio_property_row.scss";
 
+import Alarm from "../../icons/alarm";
 import { formatKPI } from "../../utils/kpi_formatters";
+import PropertyStatus from "../property_status";
+import Tooltip from "../rmb_tooltip";
 
 export default class PortfolioPropertyRow extends React.PureComponent {
   static propTypes = {
@@ -20,6 +22,10 @@ export default class PortfolioPropertyRow extends React.PureComponent {
     kpis: PropTypes.object,
     targets: PropTypes.object
   };
+
+  get isNoData() {
+    return _isNil(this.props.kpis);
+  }
 
   renderKPIs() {
     return this.props.kpi_order.map((kpi, index) => {
@@ -40,16 +46,35 @@ export default class PortfolioPropertyRow extends React.PureComponent {
     });
   }
 
+  renderNoDataTooltip() {
+    const message = (
+      <div className="portfolio-property-row__alarm-text">
+        No data found for this property during this reporting period.
+      </div>
+    );
+    if (this.isNoData) {
+      return (
+        <Tooltip placement="top" overlay={message}>
+          <div className="portfolio-property-row__alarm">
+            <Alarm className="portfolio-property-row__alarm-icon" />
+          </div>
+        </Tooltip>
+      );
+    }
+  }
+
   render() {
     const { image_url, name, address, health, type, url } = this.props;
     const imageStyle = {
       backgroundImage: `url("${image_url}")`
     };
     const classes = cn("portfolio-property-row", {
-      "portfolio-property-row--subproperty": type === "subproperty"
+      "portfolio-property-row--subproperty": type === "subproperty",
+      "portfolio-property-row--with-alarm": this.isNoData
     });
     return (
       <div className={classes}>
+        {this.renderNoDataTooltip()}
         <div className="portfolio-property-row__image" style={imageStyle} />
         <div className="portfolio-property-row__title">
           <div className="portfolio-property-row__name">{name}</div>
