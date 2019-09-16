205630e76e255c3cfce911e7ce79915f7faf8487
Alexey.Tsitrkop <alexey.tsitrkop@7bits.it>
2019-09-16 12:45:19 +0600

    Fix storybook and tests for collapsible component #167935195

--- src/js/components/collapsible/props.js
+++ src/js/components/collapsible/props.js
@@ -2,24 +2,10 @@ import Collapsible from "./index";
 import React from "react";
 
 const trigger = (
-  <Collapsible
-    trigger={
-      <div>
-        <Collapsible.Icon />
-        Collapsible row
-      </div>
-    }
-  >
-    <div>Content</div>
-    <div>Row 1</div>
-    <div>Row 2</div>
-    <div>Row 3</div>
-    <div>Row 4</div>
-    <div>Row 5</div>
-    <div>Row 6</div>
-    <div>Row 7</div>
-    <div>Row 8</div>
-  </Collapsible>
+  <div>
+    <Collapsible.Icon />
+    Collapsible row
+  </div>
 );
 
 const children = (
