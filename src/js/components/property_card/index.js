ef75e1bff6844ab174c628f5c7653d6336a2593d
Jason Child <jason@jasons-mbp.lan>
2019-09-23 11:37:35 -0700

    Use Link component for property_card instead of html anchor

--- src/js/components/property_card/index.js
+++ src/js/components/property_card/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "../button";
 import Panel from "../panel";
 import PropertyStatus from "../property_status";
+import { Link } from "react-router-dom";
 import "./property_card.scss";
 
 export const PropertyCard = ({
@@ -16,14 +17,18 @@ export const PropertyCard = ({
     imageStyle.backgroundImage = `url(${image_url})`;
     imageStyle.backgroundSize = "cover";
   }
+  console.log("property_card", url);
   return (
     <div className="property-card">
       <Panel className="property-card__panel">
         <div className="property-card__image" style={imageStyle}>
           <div className="property-card__overlay">
-            <a className="property-card__overlay-link" href={url}>
+            <Link
+              className="property-card__overlay-link"
+              to={url.replace("https://app.remarkably.io", "")}
+            >
               <Button color="outline">View Report</Button>
-            </a>
+            </Link>
             <div className="property-card__actions" />
           </div>
         </div>
