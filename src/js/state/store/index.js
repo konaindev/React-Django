2b6e97909dcbbbbf798cb90feba6d7fb2040c4cb
Jason Child <jason@jasons-mbp.lan>
2019-09-24 10:55:15 -0700

    fix store func

--- src/js/state/store/index.js
+++ src/js/state/store/index.js
@@ -34,7 +34,6 @@ export default () => {
     composeEnhancers(
       applyMiddleware(
         fetchDashboard,
-        fetchTutorial,
         fetchCreatePassword,
         fetchCompany,
         fetchCompleteAccount,
