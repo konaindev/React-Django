5182181d3b0f7aebf70facbeeb94ee9f4f4feb2b
vivianshub <53911588+vivianshub@users.noreply.github.com>
2019-10-04 09:38:25 -0700

    168391331 login page (#320)
    * login react component
    
    * function update
    
    * completed visual of login page
    
    * login view
    
    * added Login View to top level index
    
    * added input type of password
    
    * removed commented out code in test
    
    * update snapshot
    
    * updated css file to remove extraneous references already defined in base css
    
    * added checks for email and no empty password
    
    * removed Login import in top level index due to SPA cutover change
    
    * moved initialValues into constructor
    
    * removed values to data in onSubmit

---
+++ src/js/components/login/login.stories.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import LoginView from "./index";
+
+storiesOf("LoginView", module).add("default", () => <LoginView></LoginView>);
