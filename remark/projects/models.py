f6dadac2fc84a17b922a83f8af8668c1364d9867
Alexey.Tsitrkop <alexey.tsitrkop@7bits.it>
2019-10-17 13:55:43 +0900

    Make reset cache like post-save hook #168330894

--- remark/projects/models.py
+++ remark/projects/models.py
@@ -13,7 +13,6 @@ from jsonfield import JSONField
 from stdimage.models import StdImageField
 from image_cropping.utils import get_backend
 
-from remark.lib.cache import get_dashboard_cache_key, reset_cache
 from remark.lib.fields import ImageRatioFieldExt
 from remark.lib.stats import health_check
 from remark.lib.tokens import public_id
@@ -485,6 +484,9 @@ class Project(models.Model):
         users = [user.get_icon_dict() for user in users_q]
         return users
 
+    def get_project_public_id(self):
+        return self.public_id
+
     def user_can_view(self, user):
         if user.is_superuser:
             return True
@@ -506,8 +508,6 @@ class Project(models.Model):
     def save(self, *args, **kwargs):
         if not self.pk:
             self.__assign_blank_groups()
-        cache_key = get_dashboard_cache_key(self.public_id)
-        reset_cache(cache_key)
         super().save(*args, **kwargs)
 
     def __str__(self):
@@ -1099,14 +1099,12 @@ class TargetPeriod(ModelPeriod, models.Model):
     )
     target_tours.metric = SumIntervalMetric()
 
+    def get_project_public_id(self):
+        return self.project.public_id
+
     class Meta:
         # Always sort TargetPeriods with the earliest period first.
         ordering = ["start"]
-
-    def save(self, *args, **kwargs):
-        cache_key = get_dashboard_cache_key(self.project.public_id)
-        reset_cache(cache_key)
-        super().save(*args, **kwargs)
 
 
 def tam_export_media_path(instance, filename):
