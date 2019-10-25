68da08d8b996a8cb1a4d88b5d06d61be30df32f0
mingthegit <ming.dev1026@gmail.com>
2019-10-25 21:28:27 +0800

    center every storybook module in our app's Container

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,12 @@
-import { configure } from "@storybook/react";
+import React from "react";
+import { configure, addDecorator } from "@storybook/react";
 import "storybook-chromatic";
+
+import StorybookContainer from "../src/js/utils/storybook_helper";
+
+// global decorator which centers every story in the storybook
+addDecorator(storyFn => <StorybookContainer>{storyFn()}</StorybookContainer>);
+
 // automatically import all files ending in *.stories.js
 const req = require.context("../src/js", true, /.stories.js$/);
 function loadStories() {
