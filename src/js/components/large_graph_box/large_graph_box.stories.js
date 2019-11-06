fe0db00b48cf8524d441695f9e66af731ec0719e
Ben Smith <ben@remarkably.io>
2019-11-06 15:37:03 -0800

    add repro tests

--- src/js/components/large_graph_box/large_graph_box.stories.js
+++ src/js/components/large_graph_box/large_graph_box.stories.js
@@ -45,10 +45,24 @@ const props4 = {
   series: [10, 20, 30, 15]
 };
 
+const props5 = {
+  name: "USV > EXE",
+  value: 0.23,
+  target: 0.0,
+  delta: 0.01,
+  extraContent: "227 Executed Leases (Out of 260)",
+  series: [10, 20, 30, 15]
+};
+
 storiesOf("LargeGraphBox", module)
   .add("PercentageGraphBox default", () => (
     <div style={{ width: 420 }}>
       <PercentageGraphBox {...props1} />
+    </div>
+  ))
+  .add("PercentageGraphBox with zero target", () => (
+    <div style={{ width: 420 }}>
+      <PercentageGraphBox {...props5} />
     </div>
   ))
   .add("PercentageGraphBox with extra content", () => (
