5874df4133458eed48e7e0da85281f01bb1141ab
Alexander Piterimov <piterimov@gmail.com>
2019-11-07 16:12:52 +0600

    Fix migrations order #169347236

--- remark/projects/migrations/0074_property_property_url.py
+++ remark/projects/migrations/0074_property_property_url.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('projects', '0072_auto_20190919_2041'),
+        ('projects', '0073_auto_20191105_1319'),
     ]
 
     operations = [
