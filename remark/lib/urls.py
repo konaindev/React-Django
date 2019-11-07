70a40c4c9f90678edbb2640086173f01c9e95b1a
Alexander Piterimov <piterimov@gmail.com>
2019-11-07 17:32:10 +0600

    Refactor code #169347236

--- remark/lib/urls.py
+++ remark/lib/urls.py
@@ -2,10 +2,13 @@ from urllib.error import URLError
 from urllib.request import Request, urlopen
 
 
-def check_url_is_active(url):
+DEFAULT_TIMEOUT = 30
+
+
+def check_url_is_active(url, timeout=DEFAULT_TIMEOUT):
     active = True
     try:
-        with urlopen(Request(url, method="HEAD"), timeout=30) as r:
+        with urlopen(Request(url, method="HEAD"), timeout=timeout) as r:
             if r.getcode() != 200:
                 active = False
     except URLError:
