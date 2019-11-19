889094a55c634333e9972b02ca1501ad3987666e
mingthegit <ming.dev1026@gmail.com>
2019-11-19 17:28:05 +0800

    fixed api and requests for i18n strings

--- remark/web/views.py
+++ remark/web/views.py
@@ -211,7 +211,8 @@ class TutorialView(APIView):
 
 class LocalizationView(APIView):
     def post(self, request):
-        localization_form = LocalizationForm(request.data, initial={"language": "en_us"})
+        params = json.loads(request.body)
+        localization_form = LocalizationForm(params, initial={"language": "en_us"})
         if not localization_form.is_valid():
             errors = localization_form.errors.get_json_data()
             return Response(errors, status=drf_status.HTTP_500_INTERNAL_SERVER_ERROR)
