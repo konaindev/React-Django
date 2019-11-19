18ac995bcc89d146613e27360729882f408da5de
Alexander Piterimov <piterimov@gmail.com>
2019-11-19 20:19:27 +0600

    Add stories and tests #169347256

--- src/js/components/user_icon_list/user_icon_list.stories.js
+++ src/js/components/user_icon_list/user_icon_list.stories.js
@@ -4,8 +4,16 @@ import { storiesOf } from "@storybook/react";
 import UserIconList from "./index";
 import { props } from "./props";
 
-storiesOf("UserIconList", module).add("default", () => (
-  <div style={{ padding: "100px", background: "#262F38" }}>
-    <UserIconList style={{ borderColor: "#262F38" }} {...props} />
-  </div>
-));
+const containerStyle = { padding: "100px", background: "#262F38" };
+
+storiesOf("UserIconList", module)
+  .add("default", () => (
+    <div style={containerStyle}>
+      <UserIconList style={{ borderColor: "#262F38" }} {...props} />
+    </div>
+  ))
+  .add("project theme", () => (
+    <div style={containerStyle}>
+      <UserIconList theme="project" {...props} />
+    </div>
+  ));
