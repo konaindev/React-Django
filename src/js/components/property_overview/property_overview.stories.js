3ff50108b2911a7a9a9184fa489a459d6ba9c12e
Alexander Piterimov <piterimov@gmail.com>
2019-12-17 19:28:15 +0600

    Fixed `property_overview` storybook #169347219

--- src/js/components/property_overview/property_overview.stories.js
+++ src/js/components/property_overview/property_overview.stories.js
@@ -1,11 +1,17 @@
 import React from "react";
-
+import { Provider } from "react-redux";
 import { storiesOf } from "@storybook/react";
+
+import storeFunc from "../../redux_base/store";
 
 import PropertyOverview from "./index";
 import props from "./props";
 
+const { store } = storeFunc();
+const withProvider = story => <Provider store={store}>{story()}</Provider>;
+
 storiesOf("PropertyOverview", module)
+  .addDecorator(withProvider)
   .add("default", () => <PropertyOverview {...props} />)
   .add("without site and image", () => (
     <PropertyOverview
