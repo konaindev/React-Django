d76379fc0c31d9a4c6ef4be78ab70b999fd51801
Vivian Hong <vivian@Vivian.hsd1.wa.comcast.net>
2019-12-17 15:08:38 -0800

    created analytics response and referral source models

--- remark/analytics/models.py
+++ remark/analytics/models.py
@@ -30,8 +30,34 @@ class AnalyticsProvider(models.Model):
             models.UniqueConstraint(fields=["project", "provider"], name="unique_project_provider"),
         ]
 
-class GoogleAnalyticsResponse(models.Model):
+class AnalyticsResponse(models.Model):
     """
     the model for a google analytics response per viewId
     """
+
+    project_id = models.ForeignKey(
+        'projects.Project',
+        on_delete=models.CASCADE
+    )
+
+    date = models.DateField(db_index=True, help_text="The date for the data results.")
     
+
+class AnalyticsReferralSource(models.Model):
+    """
+    the model for each referral source within an analytics response
+    """
+
+    response_id = models.ForeignKey(
+        'analytics.AnalyticsResponse',
+        on_delete=models.CASCADE
+    )
+
+    source_name = models.CharField(max_length=255, help_text="The source of referrals.")
+
+    # source_url = models.CharField(max_length=255, help_text="The path of the referring URL (e.g., document.referrer).")
+
+    bounces = models.FloatField(help_text="The total number of single page (or single interaction hit) sessions for the property.")
+
+    session_duration = models.CharField(max_length=255, help_text="The length (returned as a string) of a session measured in seconds and reported in second increments.")
+
