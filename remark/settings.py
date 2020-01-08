54b8718cb9a59f16d08f2891ccec0f97240f02ea
Todd Cullen <culltp1@gmail.com>
2020-01-07 16:48:55 -0800

    forcing staging deployment

--- remark/settings.py
+++ remark/settings.py
@@ -228,6 +228,7 @@ LOGOUT_REDIRECT_URL = "/"
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = []
 
 #
 # Storages for all other files
