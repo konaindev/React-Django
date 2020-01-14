c99e4db39efc5c56c1040373af92c0645942c45b
Vivian Hong <vivian.hong.us@gmail.com>
2020-01-14 15:11:23 -0800

    testing

--- remark_airflow/insights/impl/projects/projects.py
+++ remark_airflow/insights/impl/projects/projects.py
@@ -3,7 +3,7 @@ from graphkit import compose
 # try:
 #     from insights.impl.vars import var_project
 # except ModuleNotFoundError:
-from remark_airflow.insights import var_project
+from remark_airflow.insights.impl.vars import var_project
 
 
 def get_project_facts(project_insights, project_id, start, end):
