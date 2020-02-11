4ded9c8021fec29b88f0b102c8ebe7a77a012e59
Alexander Piterimov <piterimov@gmail.com>
2020-02-11 15:46:51 +0600

    Finish data items styles #170250512

--- src/js/components/complete_account_view/index.js
+++ src/js/components/complete_account_view/index.js
@@ -64,6 +64,70 @@ const CompanyInfo = ({ onOpenCompanyModal, data }) => (
   </div>
 );
 
+const OfficeInfoEmpty = ({ onOpenOfficeModal }) => (
+  <div>
+    <div className="complete-account__section-label">Office Info</div>
+    <Button
+      className="complete-account__edit-button"
+      color="secondary-gray"
+      onClick={onOpenOfficeModal}
+    >
+      Enter Office info
+    </Button>
+  </div>
+);
+
+const OfficeInfo = ({ onOpenOfficeModal, data }) => (
+  <div>
+    <div className="complete-account__section-label">
+      Office Info
+      <Button
+        className="complete-account__edit-button complete-account__edit-button--in-box"
+        color="secondary-gray"
+        onClick={onOpenOfficeModal}
+      >
+        Edit Office Info
+      </Button>
+    </div>
+    <div className="complete-account__field-value">
+      <div className="complete-account__field-label">Country</div>
+      <div className="complete-account__value">
+        {COUNTRY_FIELDS[data.office_country.value].full_name}
+      </div>
+    </div>
+    <div className="complete-account__field-value">
+      <div className="complete-account__field-label">Address</div>
+      <div className="complete-account__value">{data.office_street}</div>
+    </div>
+    <div className="complete-account__field-value">
+      <div className="complete-account__field-label">
+        {COUNTRY_FIELDS[data.office_country.value].address_fields.city}
+      </div>
+      <div className="complete-account__value">{data.office_city}</div>
+    </div>
+    <div className="complete-account__field-value">
+      <div className="complete-account__field-label">
+        {COUNTRY_FIELDS[data.office_country.value].address_fields.state}
+      </div>
+      <div className="complete-account__value">{data.office_state.value}</div>
+    </div>
+    <div className="complete-account__field-value">
+      <div className="complete-account__field-label">
+        {COUNTRY_FIELDS[data.office_country.value].address_fields.zip}
+      </div>
+      <div className="complete-account__value">{data.office_zip}</div>
+    </div>
+    <div className="complete-account__field-value">
+      <div className="complete-account__field-label">Office Name</div>
+      <div className="complete-account__value">{data.office_name}</div>
+    </div>
+    <div className="complete-account__field-value">
+      <div className="complete-account__field-label">Type</div>
+      <div className="complete-account__value">{data.office_type.label}</div>
+    </div>
+  </div>
+);
+
 export class CompleteAccountView extends React.PureComponent {
   static propTypes = {
     office_types: Select.optionsType.isRequired,
@@ -76,7 +140,6 @@ export class CompleteAccountView extends React.PureComponent {
   constructor(props) {
     super(props);
     this.formik = React.createRef();
-    this.selectedCountry = COUNTRY_FIELDS.USA.short_name;
     this.state = {
       isCompanyOpen: false,
       isOfficeOpen: false
@@ -412,18 +475,14 @@ export class CompleteAccountView extends React.PureComponent {
                     onOpenCompanyModal={this.onOpenCompanyModal}
                   />
                 )}
-                <div>
-                  <div className="complete-account__section-label">
-                    Office Info
-                  </div>
-                  <Button
-                    className="complete-account__edit-button"
-                    color="secondary-gray"
-                    onClick={this.onOpenOfficeModal}
-                  >
-                    Enter Office info
-                  </Button>
-                </div>
+                {isTrueValues(office) ? (
+                  <OfficeInfo
+                    data={office}
+                    onOpenOfficeModal={this.onOpenOfficeModal}
+                  />
+                ) : (
+                  <OfficeInfoEmpty onOpenOfficeModal={this.onOpenOfficeModal} />
+                )}
                 <div className="complete-account__terms">
                   <Checkbox
                     className="complete-account__checkbox"
