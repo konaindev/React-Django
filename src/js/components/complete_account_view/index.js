302fd1f5e0469ac96d9d4fd3cea36b77343b5c6a
Alexander Piterimov <piterimov@gmail.com>
2020-02-14 20:29:31 +0600

    Move functional components to separate files #170250512

--- src/js/components/complete_account_view/index.js
+++ src/js/components/complete_account_view/index.js
@@ -1,5 +1,5 @@
 import cn from "classnames";
-import { Formik, Form } from "formik";
+import { Form, Formik } from "formik";
 import _intersection from "lodash/intersection";
 import _pick from "lodash/pick";
 import PropTypes from "prop-types";
@@ -27,125 +27,10 @@ import {
 } from "../../redux_base/actions";
 import { isNotBlankValues } from "../../utils/misc";
 
+import { CompanyInfo, CompanyInfoEmpty } from "./company";
+import { OfficeInfo, OfficeInfoEmpty } from "./office";
 import { propertySchema } from "./validators";
 import "./complete_account_view.scss";
-
-const CompanyInfoEmpty = ({ onOpenCompanyModal, showErrorMessage }) => (
-  <div>
-    <div className="complete-account__section-label">
-      Company Info {showErrorMessage()}
-    </div>
-    <Button
-      className="complete-account__edit-button"
-      color="secondary-gray"
-      asDiv={true}
-      onClick={onOpenCompanyModal}
-      onKeyPress={onOpenCompanyModal}
-      tabIndex="0"
-    >
-      Enter Company info
-    </Button>
-  </div>
-);
-
-const CompanyInfo = ({ data, onOpenCompanyModal, showErrorMessage }) => (
-  <div>
-    <div className="complete-account__section-label">
-      Company Info
-      {showErrorMessage()}
-      <Button
-        className="complete-account__edit-button complete-account__edit-button--in-box"
-        color="secondary-gray"
-        asDiv={true}
-        onClick={onOpenCompanyModal}
-        onKeyPress={onOpenCompanyModal}
-        tabIndex="0"
-      >
-        Enter Company info
-      </Button>
-    </div>
-    <div className="complete-account__field-value">
-      <div className="complete-account__field-label">Company</div>
-      <div className="complete-account__value">{data.company.label}</div>
-    </div>
-    <div className="complete-account__field-value">
-      <div className="complete-account__field-label">Company Role</div>
-      <div className="complete-account__value">
-        {data.company_roles.map(r => r.label).join(", ")}
-      </div>
-    </div>
-  </div>
-);
-
-const OfficeInfoEmpty = ({ onOpenOfficeModal }) => (
-  <div>
-    <div className="complete-account__section-label">Office Info</div>
-    <Button
-      className="complete-account__edit-button"
-      color="secondary-gray"
-      asDiv={true}
-      onClick={onOpenOfficeModal}
-      onKeyPress={onOpenOfficeModal}
-      tabIndex="0"
-    >
-      Enter Office info
-    </Button>
-  </div>
-);
-
-const OfficeInfo = ({ onOpenOfficeModal, data }) => (
-  <div>
-    <div className="complete-account__section-label">
-      Office Info
-      <Button
-        className="complete-account__edit-button complete-account__edit-button--in-box"
-        color="secondary-gray"
-        asDiv={true}
-        onClick={onOpenOfficeModal}
-        onKeyPress={onOpenOfficeModal}
-        tabIndex="0"
-      >
-        Edit Office Info
-      </Button>
-    </div>
-    <div className="complete-account__field-value">
-      <div className="complete-account__field-label">Country</div>
-      <div className="complete-account__value">
-        {COUNTRY_FIELDS[data.office_country.value].full_name}
-      </div>
-    </div>
-    <div className="complete-account__field-value">
-      <div className="complete-account__field-label">Address</div>
-      <div className="complete-account__value">{data.office_street}</div>
-    </div>
-    <div className="complete-account__field-value">
-      <div className="complete-account__field-label">
-        {COUNTRY_FIELDS[data.office_country.value].address_fields.city}
-      </div>
-      <div className="complete-account__value">{data.office_city}</div>
-    </div>
-    <div className="complete-account__field-value">
-      <div className="complete-account__field-label">
-        {COUNTRY_FIELDS[data.office_country.value].address_fields.state}
-      </div>
-      <div className="complete-account__value">{data.office_state.value}</div>
-    </div>
-    <div className="complete-account__field-value">
-      <div className="complete-account__field-label">
-        {COUNTRY_FIELDS[data.office_country.value].address_fields.zip}
-      </div>
-      <div className="complete-account__value">{data.office_zip}</div>
-    </div>
-    <div className="complete-account__field-value">
-      <div className="complete-account__field-label">Office Name</div>
-      <div className="complete-account__value">{data.office_name}</div>
-    </div>
-    <div className="complete-account__field-value">
-      <div className="complete-account__field-label">Type</div>
-      <div className="complete-account__value">{data.office_type.label}</div>
-    </div>
-  </div>
-);
 
 export class CompleteAccountView extends React.PureComponent {
   static propTypes = {
