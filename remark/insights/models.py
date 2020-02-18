888702c41402882c5e1710525a437893470b1d81
mingthegit <ming.dev1026@gmail.com>
2020-02-18 09:43:20 +0800

    added suggested action, tactic and kpi models

--- remark/insights/models.py
+++ remark/insights/models.py
@@ -19,6 +19,18 @@ def get_performance_insights_id():
     return public_id("performance_insights")
 
 
+def get_suggested_action_id():
+    return public_id("suggested_action")
+
+
+def get_suggested_action_tactic_id():
+    return public_id("action_tactic")
+
+
+def get_kpi_id():
+    return public_id("kpi")
+
+
 class AbstractBaseInsights(models.Model):
     project = models.ForeignKey(
         "projects.Project",
@@ -73,3 +85,104 @@ class Insight(models.Model):
 
     class Meta(object):
         ordering = ["priority_order"]
+
+
+
+class SuggestedActionManager(models.Manager):
+    pass
+
+
+class SuggestedAction(models.Model):
+    public_id = models.CharField(
+        primary_key=True, max_length=32, default=get_suggested_action_id
+    )
+
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=160)
+
+
+class SuggestedActionTacticManager(models.Manager):
+    pass
+
+
+class SuggestedActionTactic(models.Model):
+    public_id = models.CharField(
+        primary_key=True, max_length=32, default=get_suggested_action_tactic_id
+    )
+
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=160)
+    suggested_action = models.ForeignKey(
+        SuggestedAction, on_delete=models.CASCADE, related_name="tactics"
+    )
+
+
+class KPIManager(models.Manager):
+    pass
+
+
+class KPI(models.Model):
+    public_id = models.CharField(
+        primary_key=True, max_length=24, default=get_kpi_id, editable=False,
+    )
+
+    name = models.CharField(
+        max_length=255, help_text="KPI name, user cannot edit"
+    )
+    definition = models.CharField(
+        max_length=255, help_text="KPI Definition"
+    )
+
+    on_track_category_1_action = models.OneToOneField(
+        "insights.SuggestedAction",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="+",
+        help_text="Suggested Action for On Track - Category 1",
+    )
+
+    on_track_category_2_action = models.OneToOneField(
+        "insights.SuggestedAction",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="+",
+        help_text="Suggested Action for On Track - Category 2",
+    )
+
+    off_track_category_1_action = models.OneToOneField(
+        "insights.SuggestedAction",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="+",
+        help_text="Suggested Action for Off Track/At Risk - Category 1",
+    )
+
+    off_track_category_2_action = models.OneToOneField(
+        "insights.SuggestedAction",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="+",
+        help_text="Suggested Action for Off Track/At Risk - Category 2",
+    )
+
+    baseline_category_1_action = models.OneToOneField(
+        "insights.SuggestedAction",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="+",
+        help_text="Suggested Action for Baseline - Category 1",
+    )
+
+    baseline_category_2_action = models.OneToOneField(
+        "insights.SuggestedAction",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="+",
+        help_text="Suggested Action for Baseline - Category 2",
+    )
