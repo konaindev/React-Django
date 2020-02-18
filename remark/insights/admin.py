2200608f81e4a1131a145f901f9641b05ba58346
mingthegit <ming.dev1026@gmail.com>
2020-02-18 17:27:33 +0800

    initial version of kpi admin pages

--- remark/insights/admin.py
+++ remark/insights/admin.py
@@ -3,7 +3,7 @@ from remark.admin import admin_site
 
 from remark.lib.logging import error_text, getLogger
 
-from .models import Insight
+from .models import Insight, SuggestedAction, SuggestedActionTactic, KPI
 
 from adminsortable2.admin import SortableAdminMixin
 
@@ -12,3 +12,19 @@ logger = getLogger(__name__)
 @admin.register(Insight, site=admin_site)
 class InsightAdmin(SortableAdminMixin, admin.ModelAdmin):
     list_display = ["name", "include_in_email"]
+
+
+@admin.register(SuggestedAction, site=admin_site)
+class SuggestedActionAdmin(admin.ModelAdmin):
+    list_display = ["title", "description"]
+
+
+@admin.register(SuggestedActionTactic, site=admin_site)
+class SuggestedActionTacticAdmin(admin.ModelAdmin):
+    list_display = ["name", "description"]
+
+
+@admin.register(KPI, site=admin_site)
+class KPIAdmin(admin.ModelAdmin):
+    list_display = ["name", "definition"]
+    readonly_fields = ["name"]
