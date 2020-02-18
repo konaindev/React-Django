5cb03a5adb51645cdc3d3cd97620ff97700463e9
ebentz73 <evanbentz.dev@gmail.com>
2020-02-18 07:42:48 -0500

    fixed circle ci note

--- src/js/containers/error/error.stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-
-import { ErrorContainer } from "./index";
-import props from "./props";
-
-storiesOf("ErrorContainer", module).add("default", () => (
-  <ErrorContainer {...props} />
-));
